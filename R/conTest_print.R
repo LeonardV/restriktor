73d0e7dffe461b9cc15d1a3453461db4db61ac95
LeonardV <lgf.vanbrabant@gmail.com>
2016-03-02 16:08:35 +0100

    fix digits

--- R/conTest_print.R
+++ R/conTest_print.R
@@ -9,11 +9,17 @@ print.conTest <- function(x, digits = max(3, getOption("digits") - 4), ...) {
       names(out.test) <- c(" Test statistic", "p-value")
   } else if (x$test == "F") {
     cat("\nConstrained hypothesis test\n\n")
-    out.test <- c(x$Ts, x$df, x$df.residual, if (x$pvalue < 1e-04) { "<0.0001" } else { x$pvalue})
-      names(out.test) <- c(" Test statistic", "df", "df.residual", "p-value")
+#    out.test <- c(x$Ts, x$df, x$df.residual, if (x$pvalue < 1e-04) { "<0.0001" } else { x$pvalue})
+    out.test <- as.numeric(c(sprintf("%.4f", x$Ts), sprintf("%.4f", x$df), sprintf("%.4f", x$df.residual), 
+                             if (x$pvalue < 1e-04) { "<0.0001" } 
+                             else { sprintf("%.4f", x$pvalue) }))
+    names(out.test) <- c(" Test statistic", "df", "df.residual", "p-value")
   } else if (x$test == "Wald") {
     cat("\nConstrained hypothesis test\n\n")
-    out.test <- c(x$Ts, x$df, if (x$pvalue < 1e-04) { "<0.0001" } else { x$pvalue})
+#    out.test <- c(x$Ts, x$df, if (x$pvalue < 1e-04) { "<0.0001" } else { x$pvalue})
+    out.test <- as.numeric(c(sprintf("%.4f", x$Ts), sprintf("%.4f", x$df), 
+                             if (x$pvalue < 1e-04) { "<0.0001" } 
+                             else { sprintf("%.4f", x$pvalue) }))
     names(out.test) <- c(" Test statistic", "df", "p-value")
   } 
   print(out.test, digits = digits)

5c28c4c19a640bff8795e2afe11b233e97a3e4b6
LeonardV <lgf.vanbrabant@gmail.com>
2016-03-03 10:19:23 +0100

    add quote = FALSE to print()

--- R/conTest_print.R
+++ R/conTest_print.R
@@ -5,24 +5,28 @@ print.conTest <- function(x, digits = max(3, getOption("digits") - 4), ...) {
   }
   if (nrow(x$Amat) != x$meq) {
     cat("\nConstrained hypothesis test type", x$type, "\n\n")
-    out.test <- c(x$Ts, if (x$pvalue < 1e-04) { "<0.0001" } else { x$pvalue })
+    out.test <- c(sprintf("%.4f", x$Ts), 
+                  if (x$pvalue < 1e-04) { "<0.0001" } 
+                  else { sprintf("%.4f", x$pvalue) })
       names(out.test) <- c(" Test statistic", "p-value")
-  } else if (x$test == "F") {
-    cat("\nConstrained hypothesis test\n\n")
-#    out.test <- c(x$Ts, x$df, x$df.residual, if (x$pvalue < 1e-04) { "<0.0001" } else { x$pvalue})
-    out.test <- as.numeric(c(sprintf("%.4f", x$Ts), sprintf("%.4f", x$df), sprintf("%.4f", x$df.residual), 
-                             if (x$pvalue < 1e-04) { "<0.0001" } 
-                             else { sprintf("%.4f", x$pvalue) }))
-    names(out.test) <- c(" Test statistic", "df", "df.residual", "p-value")
-  } else if (x$test == "Wald") {
-    cat("\nConstrained hypothesis test\n\n")
-#    out.test <- c(x$Ts, x$df, if (x$pvalue < 1e-04) { "<0.0001" } else { x$pvalue})
-    out.test <- as.numeric(c(sprintf("%.4f", x$Ts), sprintf("%.4f", x$df), 
-                             if (x$pvalue < 1e-04) { "<0.0001" } 
-                             else { sprintf("%.4f", x$pvalue) }))
-    names(out.test) <- c(" Test statistic", "df", "p-value")
+  } else { #equality constraints only
+    if (x$test == "F") {
+      cat("\nConstrained hypothesis test\n\n")
+  #    out.test <- c(x$Ts, x$df, x$df.residual, if (x$pvalue < 1e-04) { "<0.0001" } else { x$pvalue})
+      out.test <- c(sprintf("%.4f", x$Ts), sprintf("%.4f", x$df), sprintf("%.4f", x$df.residual), 
+                               if (x$pvalue < 1e-04) { "<0.0001" } 
+                               else { sprintf("%.4f", x$pvalue) })
+      names(out.test) <- c(" Test statistic", "df", "df.residual", "p-value")
+    } else if (x$test == "Wald") {
+      cat("\nConstrained hypothesis test\n\n")
+  #    out.test <- c(x$Ts, x$df, if (x$pvalue < 1e-04) { "<0.0001" } else { x$pvalue})
+      out.test <- c(sprintf("%.4f", x$Ts), sprintf("%.4f", x$df), 
+                               if (x$pvalue < 1e-04) { "<0.0001" } 
+                               else { sprintf("%.4f", x$pvalue) })
+      names(out.test) <- c(" Test statistic", "df", "p-value")
+    }  
   } 
-  print(out.test, digits = digits)
+  print(out.test, quote = FALSE, digits = digits)
 }
 
 
--- R/conTest_summary.R
+++ R/conTest_summary.R
@@ -3,98 +3,97 @@ summary.conTest <- function(x, digits = max(3, getOption("digits") - 4), brief =
   if (!("conTest" %in% class(x))) {
     stop("x must be of class \"conTest\"")
   }
-  object <- x
-  if (nrow(object$Amat) == object$meq) {
+  if (nrow(x$Amat) == x$meq) {
     cat("\nConstrained hypothesis test\n")
   } else {
-    cat("\nConstrained hypothesis test type", object$type, "\n")  
+    cat("\nConstrained hypothesis test type", x$type, "\n")  
   }
-  vnames <- names(object$b.constr)
+  vnames <- names(x$b.constr)
   if (is.null(vnames)) { 
-    vnames <- paste("m", 1:length(object$b.constr), sep = "") 
+    vnames <- paste("m", 1:length(x$b.constr), sep = "") 
   }
-  Amat <- object$Amat
+  Amat <- x$Amat
   colnames(Amat) <- vnames
-  out.rest <- cbind(Amat, c(rep("   ==", object$meq), rep("   >=", nrow(object$Amat) -
-                                                     object$meq)), object$bvec)
+  out.rest <- cbind(Amat, c(rep("   ==", x$meq), rep("   >=", nrow(x$Amat) -
+                                                     x$meq)), x$bvec)
   
   rownames(out.rest) <- paste(1:nrow(out.rest), ":", sep = "")
   
   colnames(out.rest)[(ncol(Amat) + 1):ncol(out.rest)] <- c("op", "rhs")
   out.rest <- cbind(rep(" ", nrow(out.rest)), out.rest)
-  out.rest[object$iact, 1] <- "A"
+  out.rest[x$iact, 1] <- "A"
   out.rest <- as.data.frame(out.rest)
   names(out.rest)[1] <- ""
 
-  out.test <- as.numeric(c(sprintf("%.4f", object$Ts), 
-                                   if (object$pvalue < 1e-04) { "<0.0001" } 
-                                   else { sprintf("%.4f", object$pvalue) }))
+  out.test <- c(sprintf("%.4f", x$Ts), 
+                                   if (x$pvalue < 1e-04) { "<0.0001" } 
+                                   else { sprintf("%.4f", x$pvalue) })
   names(out.test) <- c(" Test statistic", "p-value")
 
-  if (nrow(object$Amat) != object$meq) {    
-    if (object$type == "A") {
+  if (nrow(x$Amat) != x$meq) {    
+    if (x$type == "A") {
       cat("\n H0: all constraints active (=)",
           "\n HA: at least one restriction strictly true (>)",
           "\n\n")
       print(out.test, quote = FALSE)
       if (!brief) {
-        #cat("\n\nConstraints on", vnames[colSums(!object$Amat == 0) > 0], fill = TRUE)
+        #cat("\n\nConstraints on", vnames[colSums(!x$Amat == 0) > 0], fill = TRUE)
         cat("\n\n(rows indicated with an \"A\" are active constraints)\n")
         print(out.rest, quote = FALSE, scientific = FALSE)
       }
       cat("\n Constrained estimate under H0:\n")
-      print.default(format(object$b.eqconstr, digits = digits),
+      print.default(format(x$b.eqconstr, digits = digits),
                     print.gap = 2, quote = FALSE)
       cat("\n Constrained estimate under union of H0 and HA:\n")
-      print.default(format(object$b.constr, digits = digits),
+      print.default(format(x$b.constr, digits = digits),
                     print.gap = 2, quote = FALSE)
     }
-    if (object$type == "B" && object$meq.alt == 0L) {
+    if (x$type == "B" && x$meq.alt == 0L) {
         cat("\n H0: all constraints true (>=)",
             "\n HA: at least one constraint violated (<)",
             "\n\n")
         print(out.test, quote = FALSE)
         if (!brief) {
-          #cat("\nConstraints on", vnames[colSums(!object$Amat == 0) > 0], "\n")
+          #cat("\nConstraints on", vnames[colSums(!x$Amat == 0) > 0], "\n")
           cat("\n\n(rows indicated with an \"A\" are active constraints)\n")
           print(out.rest, quote = FALSE, scientific = FALSE)
         }
         cat("\n Constrained estimate under H0:\n")
-        print.default(format(object$b.constr, digits = digits),
+        print.default(format(x$b.constr, digits = digits),
                       print.gap = 2, quote = FALSE)
         cat("\n Unconstrained estimate:\n")
-        print.default(format(object$b.unconstr, digits = digits),
+        print.default(format(x$b.unconstr, digits = digits),
                       print.gap = 2, quote = FALSE)
       }
-      else if (object$type == "B" && object$meq.alt > 0L) {
+      else if (x$type == "B" && x$meq.alt > 0L) {
         cat("\n H0: all constraints true (>= or =)",
             "\n HA: at least one constraint violated (<), some =-constraints maintained",
             "\n\n")
         print(out.test, quote = FALSE)
         if (!brief) {
-          #cat("\n\nConstraints on", vnames[colSums(!object$Amat == 0) > 0], "\n")
+          #cat("\n\nConstraints on", vnames[colSums(!x$Amat == 0) > 0], "\n")
           cat("\n\n(rows indicated with an \"A\" are active constraints)\n")
           print(out.rest, quote = FALSE, scientific = FALSE)
         }
         cat("\n Constrained estimate under H0:\n")
-        print.default(format(object$b.constr, digits = digits),
+        print.default(format(x$b.constr, digits = digits),
                       print.gap = 2, quote = FALSE)
         cat("\n Constrained estimate under HA:\n")
-        print.default(format(object$b.constr.alt, digits = digits),
+        print.default(format(x$b.constr.alt, digits = digits),
                       print.gap = 2, quote = FALSE)
       }
-      if (object$type == "C") {
+      if (x$type == "C") {
         cat("\n H0: at least one constraint not strictly true (<=)",
             "\n HA: all constraints strictly true (>)",
             "\n\n")
         print(out.test, quote = FALSE)
         if (!brief) {
-          #cat("\n\nConstraints on", vnames[colSums(!object$Amat == 0) > 0], "\n")
+          #cat("\n\nConstraints on", vnames[colSums(!x$Amat == 0) > 0], "\n")
           cat("\n\n(rows indicated with an \"A\" are active constraints)\n")
           print(out.rest, quote = FALSE, scientific = FALSE)
         }
         cat("\n Unconstrained estimate:\n")
-        print.default(format(object$b.unconstr, digits = digits),
+        print.default(format(x$b.unconstr, digits = digits),
                       print.gap = 2, quote = FALSE)
       }
   } else {
@@ -103,15 +102,15 @@ summary.conTest <- function(x, digits = max(3, getOption("digits") - 4), brief =
         "\n\n")
     print(out.test, quote = FALSE)
     if (!brief) {
-      #cat("\n\nConstraints on", vnames[colSums(!object$Amat == 0) > 0], fill = TRUE)
+      #cat("\n\nConstraints on", vnames[colSums(!x$Amat == 0) > 0], fill = TRUE)
       cat("\n\n(rows indicated with an \"A\" are active constraints)\n")
       print(out.rest, quote = FALSE, scientific = FALSE)
     }
     cat("\n Constrained estimate under H0:\n")
-    print.default(format(object$b.constr, digits = digits),
+    print.default(format(x$b.constr, digits = digits),
                   print.gap = 2, quote = FALSE)
     cat("\n Constrained estimate under union of H0 and HA:\n")
-    print.default(format(object$b.unconstr, digits = digits),
+    print.default(format(x$b.unconstr, digits = digits),
                   print.gap = 2, quote = FALSE)
     }
 }

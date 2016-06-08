d6317ee362718633423f8f642394574d21664245
LeonardV <lgf.vanbrabant@gmail.com>
2016-06-08 09:44:54 +0200

    code cleanup

--- R/conTest.R
+++ R/conTest.R
@@ -4,10 +4,12 @@ conTest <- function(object, type = "summary", ...) {
     stop("object must be of class \"conLM\" or \"conRLM\"")
   }
   
-  Amat <- object$constraints
-  meq <- object$neq
+  stopifnot(type %in% c("A","B","global","summary"))
   
   l <- list(...)
+  Amat <- object$constraints
+  meq  <- object$neq
+  
   if (nrow(Amat) > meq) {
     if (!("test" %in% names(l))) {
       test <- "F"
@@ -34,34 +36,31 @@ conTest <- function(object, type = "summary", ...) {
                      " elements cannot be created", sep = ""))
       }
     } 
-    # check 
-    if (!is.null(l$control)) {
-      if ("B" %in% names(l$control)) {
-        if (l$control$B < 9999) {
-          warning("The number of bootstrap samples for computing the mixing weights is too low and may cause spurious results (by default B = 99999).")
-        }
-      }
-    }
     
     # check
-    if (class(object)[1] == "conLM" && !(test %in% c("F","Wald","LRT","score"))) {
-      stop("restriktor ERROR: test ", sQuote(test), " unknown. Choose F, LRT or score.")  
-    } else if (class(object)[1] == "conRLM" && !(test %in% c("F","Wald","score"))) {
-      stop("restriktor ERROR: test ", sQuote(test), " unknown. Choose F, Wald or score.")  
-    } 
     if (type %in% c("A","B","global")) {
-      if (test == "F") {
-        UseMethod("conTestF")
-      } else if (test == "LRT") {
-        UseMethod("conTestLRT")
-      } else if (test == "score") {
-        UseMethod("conTestScore")
-      } else if (test == "Wald") {
-         if (class(object)[1] == "conLM") {
-           UseMethod("conTestF")
-         } else if (class(object)[1] == "conRLM") {
-           UseMethod("conTestWald")
-         }
+      if (class(object)[1] == "conLM") {
+        if (!(test %in% c("F","LRT","score"))) {
+          stop("restriktor ERROR: test ", sQuote(test), " unknown. Choose F, LRT or score.")  
+        } 
+        if (test == "F") {
+          UseMethod("conTestF")
+        } else if (test == "LRT") {
+          UseMethod("conTestLRT")
+        } else if (test == "score") {
+          UseMethod("conTestScore")
+        }
+      } else if (class(object)[1] == "conRLM") { 
+        if (!(test %in% c("F","Wald","score"))) {
+          stop("restriktor ERROR: test ", sQuote(test), " unknown. Choose F, Wald or score.")  
+        } 
+        if (test == "F") {
+          UseMethod("conTestF")
+        } else if (test == "Wald") {
+          UseMethod("conTestWald")
+        } else if (test == "score") {
+          UseMethod("conTestScore")
+        } 
       }
     } else if (type == "C") {
       UseMethod("conTestC")

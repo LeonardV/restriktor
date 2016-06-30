e99ab1585cd9fd22612b8fc508b96b300784da4b
leonardv <lgf.vanbrabant@gmail.com>
2016-06-30 16:33:53 +0200

    change model to object

--- R/restriktor.R
+++ R/restriktor.R
@@ -1,24 +1,24 @@
 ################################### TO DO ######################################
 # - add weights + down weights to RLM.
 # - implement E-bar test statistic for lm.
-# - add support for no intercept models.
+# - add support for no intercept objects.
 # - add small sample correction for GORIC, see REF. above.
 # - add support for mlm and glm.
 # - add test-statistic for class of rlm with equality restriktions only.
 # - add weights to conTestEq
 # - check for right function arguments
 ###############################################################################
-restriktor <- function(model, constraints = NULL, ...) {
+restriktor <- function(object, constraints = NULL, ...) {
   
   # check the class of object
-  if (!any(class(model) %in% c("lm", "rlm"))) {
+  if (!any(class(object) %in% c("lm", "rlm"))) {
     stop("restriktor only works for lm(), rlm()")
   }
   
-  if (class(model)[1] %in% c("lm","mlm")) {
+  if (class(object)[1] %in% c("lm","mlm")) {
     UseMethod("conLM")
   } 
-  else if (class(model)[1] %in% "rlm") {
+  else if (class(object)[1] %in% "rlm") {
     UseMethod("conRLM")
   }
   

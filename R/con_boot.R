9e56e506cf4ecc85a4902b2d0597a7b907e91298
LeonardV <lgf.vanbrabant@gmail.com>
2016-10-13 16:39:35 +0200

    code cleanup

--- R/con_boot.R
+++ R/con_boot.R
@@ -19,27 +19,3 @@ con_boot_lm <- function(object, B = 999, fixed = FALSE, ...) {
   
     OUT
 }
-
-
-# con_boot_rlm <- function(object, B = 999, fixed = FALSE, ...) { 
-#   
-#   if (class(object)[1] != "rlm") {
-#     stop("ERROR: object must be of class rlm")
-#   }
-#   DATA <- as.data.frame(object$model)
-#   if (!fixed) { # standard bootstrap
-#     bootout <- boot(DATA,  
-#                     con_bootdata_lm, 
-#                     R = B, ...)
-#   } else { # model based bootstrap
-#     res <- object$residuals
-#     fit <- object$fitted.values
-#     bootout <- boot(data.frame(DATA, fit = fit, res = res), 
-#                     con_boot_fixed_lm, R = B, ...)
-#   }
-#   
-#   OUT <- bootout
-#   
-#   OUT
-# }
-

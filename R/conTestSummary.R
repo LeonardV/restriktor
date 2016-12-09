b0e47aaf3efa739ce62991adaf83d6f7d922442c
LeonardV <lgf.vanbrabant@gmail.com>
2016-12-09 15:39:19 +0100

    added function to compute all available hypothesis tests

---
+++ R/conTestSummary.R
@@ -0,0 +1,31 @@
+conTestSummary.conLM <- function(object, test = "F", ...) {
+  
+  x <- object
+  if (!(inherits(object, c("conLM","conRLM")))) {
+    stop("Restriktor ERROR: object must be of class \"conLM\" or \"conRLM\"")
+  }
+  
+  ldots <- list(...)
+  CALL <- c(list(object = x, test = test), ldots)
+  
+  OUT <- list()
+  # fit all available hypothesis tests
+  CALL$type <- "global"
+  OUT1 <- do.call("conTest", CALL)
+  CALL$type <- "A"
+  OUT2 <- do.call("conTest", CALL)
+  CALL$type <- "B"
+  OUT3 <- do.call("conTest", CALL)
+  
+  OUT <- c(OUT1, OUT2, OUT3)
+  
+  if (x$neq == 0) {
+    CALL$type <- "C"
+    OUT4 <- do.call("conTest", CALL)
+    OUT <- c(OUT, OUT4)
+  }
+  
+  class(OUT) <- c("conTest")
+  
+  OUT
+}

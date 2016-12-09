4acd1c2165aacc016daa486662104b84594aa345
LeonardV <lgf.vanbrabant@gmail.com>
2016-12-09 15:42:04 +0100

    changed to goric

---
+++ R/con_goric.R
@@ -0,0 +1,24 @@
+goric <- function(object, ..., digits = max(3, getOption("digits") - 2)) {
+  if (inherits(object, "conLM")) {
+    objectlist <- list(object, ...)
+  } else {
+    objectlist <- object
+  }
+  isconLM <- sapply(objectlist, function(x) inherits(x, "conLM"))
+  conlist <- objectlist[isconLM]  
+  isSummary <- lapply(conlist, function(x) summary(x))
+  
+  ll    <- unlist(lapply(isSummary, function(x) attr(x$goric, "loglik")))
+  PT    <- unlist(lapply(isSummary, function(x) attr(x$goric, "penalty")))
+  goric <- unlist(lapply(isSummary, function(x) x$goric[1]))
+  df    <- data.frame(loglik = ll, penalty = PT, goric)
+  
+  delta <- df$goric - min(df$goric)
+  goric_weights <- exp(-delta / 2) / sum(exp(-delta / 2))
+  df$goric_weights <- goric_weights
+  
+  print(format(df, digits = digits, scientific = FALSE), 
+        print.gap = 2, quote = FALSE)
+  
+  invisible(df)
+}

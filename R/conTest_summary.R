6320a50a95265ab984b74194d7c18613f4babdb1
LeonardV <lgf.vanbrabant@gmail.com>
2016-12-22 11:51:57 +0100

    added check

--- R/conTest_summary.R
+++ R/conTest_summary.R
@@ -1,12 +1,17 @@
 conTest_summary.restriktor <- function(object, test = "F", ...) {
   
-  x <- object
   if (!(inherits(object, "restriktor"))) {
     stop("Restriktor ERROR: object must be of class restriktor")
   }
   
+  Amat <- object$constraints
+  meq  <- object$neq
+  if (meq == nrow(Amat)) {
+    stop("Restriktor ERROR: test not applicable for object with equality restrictions only.")
+  }
+  
   ldots <- list(...)
-  CALL <- c(list(object = x, test = test), ldots)
+  CALL <- c(list(object = object, test = test), ldots)
   
   OUT <- list()
   # fit all available hypothesis tests
@@ -19,7 +24,7 @@ conTest_summary.restriktor <- function(object, test = "F", ...) {
   
   OUT <- c(OUT1, OUT2, OUT3)
   
-  if (x$neq == 0) {
+  if (meq == 0) {
     CALL$type <- "C"
     OUT4 <- do.call("conTest", CALL)
     OUT <- c(OUT, OUT4)

3f076d2efcf7521b6af4b5f51538ea302ccdb423
LeonardV <lgf.vanbrabant@gmail.com>
2016-12-23 13:43:15 +0100

    changed restrikted in restricted

--- R/restriktor_print.R
+++ R/restriktor_print.R
@@ -4,9 +4,9 @@ print.restriktor <- function(x, digits = max(3, getOption("digits") - 2),
   cat("\nCall:\n", paste(deparse(x$call), sep = "\n", collapse = "\n"), "\n", sep = "")
   
   if (!inherits(x, "conRLM")) {
-    cat("\nRestriktor: restrikted linear model:\n\n")
+    cat("\nRestriktor: restricted linear model:\n\n")
   } else if (inherits(x,"conRLM")) {
-    cat("\nRestriktor: restrikted robust linear model:\n\n")
+    cat("\nRestriktor: restricted robust linear model:\n\n")
   }
   
   if (length(coef(x)) > 0L) {

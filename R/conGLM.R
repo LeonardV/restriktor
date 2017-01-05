c06ac006bf91a3c6d8a2c502d1b2c237309a7617
LeonardV <lgf.vanbrabant@gmail.com>
2017-01-05 12:06:58 +0100

    add constrained generalized linear models

---
+++ R/conGLM.R
@@ -0,0 +1,342 @@
+conGLM.glm <- function(object, constraints = NULL, se = "standard", 
+                       B = 999, rhs = NULL, neq = 0L, Wt = TRUE, 
+                       bootWt = FALSE, bootWt.R = 99999,
+                       parallel = "no", ncpus = 1L, cl = NULL, 
+                       seed = NULL, control = list(), verbose = FALSE, 
+                       debug = FALSE, ...) {
+    
+  # check class
+  if (!(class(object)[1] %in% c("glm"))) {
+    stop("Restriktor ERROR: object must be of class glm.")
+  }
+  
+  # timing
+  start.time0 <- start.time <- proc.time()[3]; timing <- list()
+  
+  # store call
+  mc <- match.call()
+  
+  # rename for internal use
+  Amat <- constraints
+  bvec <- rhs 
+  meq  <- neq
+  
+  # response variable
+  y <- as.matrix(object$model[, attr(object$terms, "response")])
+  # model matrix
+  X <- model.matrix(object)[,,drop = FALSE]
+  # variance-covariance matrix
+  W <- vcov(object)
+  # familiy and link function
+  family <- object$family
+  # model summary
+  so <- summary(object)
+  # weigths
+  weights <- weights(object)
+  # unconstrained estimates
+  b_unrestr <- coef(object)
+  # number of parameters
+  p <- length(coef(object))
+  # sample size
+  n <- dim(X)[1]
+  
+  timing$preparation <- (proc.time()[3] - start.time)
+  start.time <- proc.time()[3]
+  
+  # deal with constraints
+  if (!is.null(constraints)) {
+    restr_OUT <- con_constraints(object, 
+                                 constraints = Amat, 
+                                 bvec        = bvec, 
+                                 meq         = meq, 
+                                 debug       = debug)  
+    # a list with useful information about the restriktions.}
+    CON <- restr_OUT$CON
+    # a parameter table with information about the observed variables in the object 
+    # and the imposed restriktions.}
+    parTable <- restr_OUT$parTable
+    # constraints matrix
+    Amat <- restr_OUT$Amat
+    # rhs 
+    bvec <- restr_OUT$bvec
+    # neq
+    meq <- restr_OUT$meq
+  } else if (is.null(constraints)) { 
+    # no constraints specified - needed for GORIC to include unconstrained model
+    CON <- NULL
+    parTable <- NULL
+    Amat <- rbind(rep(0L, p))
+    bvec <- rep(0L, nrow(Amat))
+    meq  <- 0L
+  } 
+  
+  # compute the reduced row-echelon form of the constraints matrix
+  rAmat <- GaussianElimination(t(Amat))
+  if (Wt && !bootWt) {
+    if (rAmat$rank < nrow(Amat) && rAmat$rank != 0L) {
+      stop(paste("Restriktor ERROR: The constraint matrix must have full row-rank ( choose e.g. rows", 
+                 paste(rAmat$pivot, collapse = " "), ", or try to set bootWt = TRUE)"))
+    }
+  } else {
+    if (rAmat$rank < nrow(Amat) && 
+        !(se %in% c("none", "boot.model.based", "boot.standard")) && 
+        rAmat$rank != 0L) {
+      se <- "none"
+      warning(paste("Restriktor Warning: No standard errors could be computed. 
+                    The constraint matrix must have full row-rank ( choose e.g. rows", 
+                    paste(rAmat$pivot, collapse = " "), "). 
+                    Try to set se = \"boot.model.based\" or \"boot.standard\"."))  
+    }
+  }
+  
+  timing$constraints <- (proc.time()[3] - start.time)
+  start.time <- proc.time()[3]
+  # parallel housekeeping
+  have_mc <- have_snow <- FALSE
+  if (parallel != "no" && ncpus > 1L) {
+    if (parallel == "multicore") {
+      have_mc <- .Platform$OS.type != "windows"
+    } else if (parallel == "snow") {
+      have_snow <- TRUE
+    }  
+    if (!have_mc && !have_snow) {
+      ncpus <- 1L
+    }  
+  }
+  
+  # standard error methods
+  if (se == "default") {
+    se <- "standard"
+  } else if (se == "boot.residual") {
+    se <- "boot.model.based"
+  }
+  if (!(se %in% c("none","standard","const","boot.model.based","boot.standard",
+                  "HC","HC0","HC1","HC2","HC3","HC4","HC4m","HC5"))) {
+    stop("Restriktor ERROR: standard error method ", sQuote(se), " unknown.")
+  }
+  if (attr(object$terms, "intercept")) {
+    if (se == "boot.model.based" & any(Amat[,1] == 1)) { 
+      stop("Restriktor ERROR: no restriktions on intercept possible",
+           "\nfor 'se = boot.model.based' bootstrap method.")
+    }
+  }
+  if(ncol(Amat) != length(b_unrestr)) {
+    stop("Restriktor ERROR: length coefficients and the number of",
+         "\ncolumns constraints-matrix must be identical")
+  }
+  
+  # unrestricted (weighted) log-likelihood
+  LL_unc <- con_loglik_glm(object)
+  
+  is.augmented <- TRUE
+  # compute mixing weights
+  if (Wt) {
+    # unrestrikted case
+    if (all(c(Amat) == 0)) { 
+      wt <- c(rep(0L, p), 1)
+      is.augmented <- FALSE
+    } else if (bootWt) { 
+      # compute mixing weights based on simulation
+      wt <- con_weights_boot(VCOV     = W,
+                             Amat     = Amat, 
+                             meq      = meq, 
+                             R        = bootWt.R,
+                             parallel = parallel,
+                             ncpus    = ncpus,
+                             cl       = cl,
+                             seed     = seed,
+                             verbose  = verbose)
+    } else if (!bootWt & (meq < nrow(Amat))) {
+      # compute mixing weights based on mvtnorm
+      wt <- rev(con_weights(Amat %*% W %*% t(Amat), meq = meq))
+    } else if (!bootWt & (meq == nrow(Amat))) {
+      # only equality constraints
+      wt <- rep(0L, ncol(W) + 1)
+      wt.idx <- ncol(W) - meq + 1
+      wt[wt.idx] <- 1
+    }
+    attr(wt, "bootWt") <- bootWt
+    if (bootWt) { attr(wt, "bootWt.R") <- bootWt.R }
+  } else {
+    wt <- NULL
+  }
+  
+  timing$wt <- (proc.time()[3] - start.time)
+  start.time <- proc.time()[3]
+  
+  # check if the constraints are not in line with the data, else skip optimization
+  if (all(Amat %*% c(b_unrestr) - bvec >= 0 * bvec) & meq == 0) {
+    b_restr <- b_unrestr
+    dispersion_restr <- so$dispersion
+    
+    OUT <- list(CON               = CON,
+                call              = mc,
+                timing            = timing,
+                parTable          = parTable,
+                family            = object$family,
+                wt                = wt,
+                b_unrestr         = b_unrestr,
+                b_restr           = b_unrestr,
+                residuals         = object$residuals, # unweighted residuals
+                fitted            = object$fitted.values,
+                weights           = object$weights,
+                df.residual       = object$df.residual,
+                df.residual_null  = object$df.null,
+                dispersion        = so$dispersion, 
+                dispersion_restr  = so$dispersion, 
+                loglik            = LL_unc,
+                aic               = object$aic,
+                deviance_null     = object$null.deviance,
+                deviance          = object$deviance,
+                Sigma             = W,
+                constraints       = Amat, 
+                rhs               = bvec, 
+                neq               = meq, 
+                iact              = 0L, 
+                converged         = object$converged,
+                bootout           = NULL, 
+                control           = control)  
+  } else {
+    ## compute constrained estimates for glm()
+    
+    # collect all original model arguments and add constraints
+    call_org <- as.list(object$call)
+    
+    CALL <- c(list(x = X, y = y, weights = weights, 
+                   start = coef(object), etastart = call_org[["etastart"]],
+                   mustart = call_org[["mustart"]], offset = object$offset,
+                   family = family, control = call_org[["control"]],
+                   intercept = attr(object$terms, "intercept"),
+                   Amat = Amat, bvec = bvec, meq = meq))
+    
+    if (is.null(CALL$control)) { CALL$control <- list() }
+    
+    # fit restricted generalized liner model
+    fit_glmc <- do.call("conGLM_fit", CALL)
+    
+    # restricted estimates
+    b_restr <- fit_glmc$coefficients
+    b_restr[abs(b_restr) < ifelse(is.null(control$tol), 
+                                  sqrt(.Machine$double.eps), 
+                                  control$tol)] <- 0L
+    
+    timing$optim <- (proc.time()[3] - start.time)
+    start.time <- proc.time()[3]
+    
+    # compute restricted dispersion
+    df.residual <- n - (p - qr(Amat[0:meq,])$rank)
+    
+    if (fit_glmc$family$family %in% c("poisson", "binomial")) {
+      dispersion_restr <- 1
+    } else {
+      if (any(fit_glmc$weights == 0)) {
+        warning("observations with zero weight not used for calculating dispersion")
+      }
+      dispersion_restr <- sum((fit_glmc$weights * 
+                                 fit_glmc$residuals^2)[fit_glmc$weights > 0]) / df.residual
+    }
+    
+    # restricted log-likelihood
+    LL_restr <- con_loglik_glm(fit_glmc)
+
+    b_restr <- as.vector(b_restr)
+      names(b_restr) <- names(b_unrestr)
+    fitted <- fit_glmc$fitted.values
+    residuals <- fit_glmc$residuals
+    
+    
+    OUT <- list(CON               = CON,
+                call              = mc,
+                timing            = timing,
+                parTable          = parTable,
+                family            = fit_glmc$family,
+                wt                = wt,
+                b_unrestr         = b_unrestr,
+                b_restr           = b_restr,
+                residuals         = residuals, # unweighted residuals
+                fitted            = fitted,
+                weights           = weights,
+                df.residual       = object$df.residual,
+                df.residual_null  = fit_glmc$df.null,
+                dispersion        = so$dispersion, 
+                dispersion_restr  = dispersion_restr, 
+                loglik            = LL_restr,
+                aic               = fit_glmc$aic,
+                deviance_null     = fit_glmc$null.deviance,
+                deviance          = fit_glmc$deviance,
+                Sigma             = W,
+                constraints       = Amat, 
+                rhs               = bvec, 
+                neq               = meq, 
+                iact              = fit_glmc$iact,
+                converged         = fit_glmc$converged,
+                bootout           = NULL, 
+                control           = control)
+  }
+  
+  # original object
+  OUT$model_org <- object
+  # type standard error
+  OUT$se <- se
+  # compute standard errors based on the augmented inverted information matrix or
+  # based on the standard bootstrap or model.based bootstrap
+  if (se != "none") {
+    if (!(se %in% c("boot.model.based","boot.standard"))) {
+      p1 <- 1L:p
+      Qr <- qr(object)
+      covmat_unscaled <- chol2inv(Qr$qr[p1, p1, drop = FALSE])
+      OUT$information <- 1 / dispersion_restr * solve(covmat_unscaled)
+      
+      information.inv <- con_augmented_information(information  = OUT$information,
+                                                   is.augmented = is.augmented,
+                                                   X            = X, 
+                                                   b_unrestr    = b_unrestr, 
+                                                   b_restr      = b_restr,
+                                                   Amat         = Amat, 
+                                                   bvec         = bvec, 
+                                                   meq          = meq) 
+      
+      attr(OUT$information, "inverted")  <- information.inv$information
+      attr(OUT$information, "inverted.augmented") <- information.inv$information.augmented
+      
+      timing$inv_aug_information <- (proc.time()[3] - start.time)
+      start.time <- proc.time()[3]
+    } else if (se == "boot.model.based") {
+      OUT$bootout <- con_boot_lm(object      = object, 
+                                 B           = B, 
+                                 fixed       = TRUE, 
+                                 constraints = Amat,
+                                 rhs         = bvec, 
+                                 neq         = meq, 
+                                 se          = "none",
+                                 bootWt      = bootWt,
+                                 bootWt.R    = bootWt.R,
+                                 parallel    = parallel, 
+                                 ncpus       = ncpus, 
+                                 cl          = cl)
+      timing$boot_model_based <- (proc.time()[3] - start.time)
+      start.time <- proc.time()[3]
+    } else if (se == "boot.standard") {
+      OUT$bootout <- con_boot_lm(object      = object, 
+                                 B           = B, 
+                                 fixed       = FALSE, 
+                                 constraints = Amat,
+                                 rhs         = bvec, 
+                                 neq         = meq, 
+                                 se          = "none",
+                                 bootWt      = bootWt,
+                                 bootWt.R    = bootWt.R,
+                                 parallel    = parallel, 
+                                 ncpus       = ncpus, 
+                                 cl          = cl)
+      timing$boot_standard <- (proc.time()[3] - start.time)
+      start.time <- proc.time()[3]
+    }
+  }
+  
+  OUT$timing$total <- (proc.time()[3] - start.time0)
+  
+  class(OUT) <- c("restriktor", "conGLM")
+  
+  OUT
+}
---
+++ R/conGLM_fit.R
@@ -0,0 +1,244 @@
+conGLM_fit <- function(x, y, weights = rep(1, nobs), start = NULL, 
+                       etastart = NULL, mustart = NULL, 
+                       offset = rep(0, nobs), family = gaussian(), 
+                       control = list(), intercept = TRUE, 
+                       Amat, bvec, meq) {
+  
+  control <- do.call("glm.control", control)
+  x <- as.matrix(x)
+  xnames <- dimnames(x)[[2L]]
+  ynames <- if (is.matrix(y)) 
+    rownames(y)
+  else names(y)
+  conv <- FALSE
+  nobs <- NROW(y)
+  nvars <- ncol(x)
+  EMPTY <- nvars == 0
+  if (is.null(weights)) 
+    weights <- rep.int(1, nobs)
+  if (is.null(offset)) 
+    offset <- rep.int(0, nobs)
+  variance <- family$variance
+  linkinv <- family$linkinv
+  if (!is.function(variance) || !is.function(linkinv)) 
+    stop("'family' argument seems not to be a valid family object", 
+         call. = FALSE)
+  dev.resids <- family$dev.resids
+  aic <- family$aic
+  mu.eta <- family$mu.eta
+  unless.null <- function(x, if.null) if (is.null(x)) 
+    if.null
+  else x
+  valideta <- unless.null(family$valideta, function(eta) TRUE)
+  validmu <- unless.null(family$validmu, function(mu) TRUE)
+  if (is.null(mustart)) {
+    eval(family$initialize)
+  }
+  else {
+    mukeep <- mustart
+    eval(family$initialize)
+    mustart <- mukeep
+  }
+  if (EMPTY) {
+    eta <- rep.int(0, nobs) + offset
+    if (!valideta(eta)) 
+      stop("invalid linear predictor values in empty model", 
+           call. = FALSE)
+    mu <- linkinv(eta)
+    if (!validmu(mu)) 
+      stop("invalid fitted means in empty model", call. = FALSE)
+    dev <- sum(dev.resids(y, mu, weights))
+    w <- sqrt((weights * mu.eta(eta)^2)/variance(mu))
+    residuals <- (y - mu)/mu.eta(eta)
+    good <- rep_len(TRUE, length(residuals))
+    boundary <- conv <- TRUE
+    coef <- numeric()
+    iter <- 0L
+  }
+  else {
+    coefold <- NULL
+    eta <- if (!is.null(etastart)) 
+      etastart
+    else if (!is.null(start)) 
+      if (length(start) != nvars) 
+        stop(gettextf("length of 'start' should equal %d and correspond to initial coefs for %s", 
+                      nvars, paste(deparse(xnames), collapse = ", ")), 
+             domain = NA)
+    else {
+      coefold <- start
+      offset + as.vector(if (NCOL(x) == 1L) 
+        x * start
+        else x %*% start)
+    }
+    else family$linkfun(mustart)
+    mu <- linkinv(eta)
+    if (!(validmu(mu) && valideta(eta))) 
+      stop("cannot find valid starting values: please specify some", 
+           call. = FALSE)
+    devold <- sum(dev.resids(y, mu, weights))
+    boundary <- conv <- FALSE
+    
+    #############################################################
+    for (iter in 1L:control$maxit) {
+      good <- weights > 0
+      varmu <- variance(mu)[good]
+      if (anyNA(varmu)) 
+        stop("NAs in V(mu)")
+      if (any(varmu == 0)) 
+        stop("0s in V(mu)")
+      mu.eta.val <- mu.eta(eta)
+      if (any(is.na(mu.eta.val[good]))) 
+        stop("NAs in d(mu)/d(eta)")
+      good <- (weights > 0) & (mu.eta.val != 0)
+      if (all(!good)) {
+        conv <- FALSE
+        warning(gettextf("no observations informative at iteration %d", 
+                         iter), domain = NA)
+        break
+      }
+      z <- (eta - offset)[good] + (y - mu)[good]/mu.eta.val[good]
+      w <- sqrt((weights[good] * mu.eta.val[good]^2)/variance(mu)[good])
+      #fit <- .Call(C_Cdqrls, x[good, , drop = FALSE] * 
+      #               w, z * w, min(1e-07, control$epsilon/1000), check = FALSE)
+      fit <- con_solver(X = x[good, , drop = FALSE], y = z, 
+                        b_unrestr = lm.fit(x, y)$coefficients, w = w, Amat = Amat, bvec = bvec, 
+                        meq = meq, maxit = control$maxit, absval = control$epsilon)
+      if (any(!is.finite(fit$coefficients))) {
+        conv <- FALSE
+        warning(gettextf("non-finite coefficients at iteration %d", 
+                         iter), domain = NA)
+        break
+      }
+      # if (nobs < fit$rank) 
+      #   stop(sprintf(ngettext(nobs, "X matrix has rank %d, but only %d observation", 
+      #                         "X matrix has rank %d, but only %d observations"), 
+      #                fit$rank, nobs), domain = NA)
+      # start[fit$pivot] <- fit$coefficients
+      start <- fit$solution
+      eta <- drop(x %*% start)
+      mu <- linkinv(eta <- eta + offset)
+      dev <- sum(dev.resids(y, mu, weights))
+      if (control$trace) 
+        cat("Deviance = ", dev, " Iterations - ", iter, 
+            "\n", sep = "")
+      boundary <- FALSE
+      if (!is.finite(dev)) {
+        if (is.null(coefold)) 
+          stop("no valid set of coefficients has been found: please supply starting values", 
+               call. = FALSE)
+        warning("step size truncated due to divergence", 
+                call. = FALSE)
+        ii <- 1
+        while (!is.finite(dev)) {
+          if (ii > control$maxit) 
+            stop("inner loop 1; cannot correct step size", 
+                 call. = FALSE)
+          ii <- ii + 1
+          start <- (start + coefold)/2
+          eta <- drop(x %*% start)
+          mu <- linkinv(eta <- eta + offset)
+          dev <- sum(dev.resids(y, mu, weights))
+        }
+        boundary <- TRUE
+        if (control$trace) 
+          cat("Step halved: new deviance = ", dev, "\n", 
+              sep = "")
+      }
+      if (!(valideta(eta) && validmu(mu))) {
+        if (is.null(coefold)) 
+          stop("no valid set of coefficients has been found: please supply starting values", 
+               call. = FALSE)
+        warning("step size truncated: out of bounds", 
+                call. = FALSE)
+        ii <- 1
+        while (!(valideta(eta) && validmu(mu))) {
+          if (ii > control$maxit) 
+            stop("inner loop 2; cannot correct step size", 
+                 call. = FALSE)
+          ii <- ii + 1
+          start <- (start + coefold)/2
+          eta <- drop(x %*% start)
+          mu <- linkinv(eta <- eta + offset)
+        }
+        boundary <- TRUE
+        dev <- sum(dev.resids(y, mu, weights))
+        if (control$trace) 
+          cat("Step halved: new deviance = ", dev, "\n", 
+              sep = "")
+      }
+      if (abs(dev - devold)/(0.1 + abs(dev)) < control$epsilon) {
+        conv <- TRUE
+        coef <- start
+        break
+      }
+      else {
+        devold <- dev
+        coef <- coefold <- start
+      }
+    }
+    
+    #############################################################
+    if (!conv) 
+      warning("glm.fit: algorithm did not converge", call. = FALSE)
+    if (boundary) 
+      warning("glm.fit: algorithm stopped at boundary value", 
+              call. = FALSE)
+    eps <- 10 * .Machine$double.eps
+    if (family$family == "binomial") {
+      if (any(mu > 1 - eps) || any(mu < eps)) 
+        warning("glm.fit: fitted probabilities numerically 0 or 1 occurred", 
+                call. = FALSE)
+    }
+    if (family$family == "poisson") {
+      if (any(mu < eps)) 
+        warning("glm.fit: fitted rates numerically 0 occurred", 
+                call. = FALSE)
+    }
+    # if (fit$rank < nvars) 
+    # coef[fit$pivot][seq.int(fit$rank + 1, nvars)] <- NA
+    xxnames <- xnames#[fit$pivot]
+    residuals <- (y - mu)/mu.eta(eta)
+    # fit$qr <- as.matrix(fit$qr)
+    nr <- min(sum(good), nvars)
+    # if (nr < nvars) {
+    #   Rmat <- diag(nvars)
+    #   Rmat[1L:nr, 1L:nvars] <- fit$qr[1L:nr, 1L:nvars]
+    # }
+    # else Rmat <- fit$qr[1L:nvars, 1L:nvars]
+    # Rmat <- as.matrix(Rmat)
+    # Rmat[row(Rmat) > col(Rmat)] <- 0
+    names(coef) <- xnames
+    # colnames(fit$qr) <- xxnames
+    # dimnames(Rmat) <- list(xxnames, xxnames)
+  }
+  names(residuals) <- ynames
+  names(mu) <- ynames
+  names(eta) <- ynames
+  wt <- rep.int(0, nobs)
+  wt[good] <- w^2
+  names(wt) <- ynames
+  names(weights) <- ynames
+  names(y) <- ynames
+  # if (!EMPTY) 
+  #   names(fit$effects) <- c(xxnames[seq_len(fit$rank)], rep.int("", 
+  #                                                               sum(good) - fit$rank))
+  wtdmu <- if (intercept) 
+    sum(weights * y)/sum(weights)
+  else linkinv(offset)
+  nulldev <- sum(dev.resids(y, wtdmu, weights))
+  n.ok <- nobs - sum(weights == 0)
+  nulldf <- n.ok - as.integer(intercept)
+  rank <- if (EMPTY) 0
+  else qr(x)$rank
+  resdf <- n.ok - rank
+  aic.model <- aic(y, n.ok, mu, weights, dev) #+ 2 * rank
+  list(coefficients = coef, residuals = residuals, fitted.values = mu, 
+       # effects = if (!EMPTY) fit$effects, R = if (!EMPTY) Rmat, 
+       rank = rank, 
+       # qr = if (!EMPTY) structure(fit[c("qr", "rank", "qraux", "pivot", "tol")], class = "qr"), 
+       family = family, 
+       linear.predictors = eta, deviance = dev, aic = aic.model, 
+       null.deviance = nulldev, iter = iter, weights = wt, prior.weights = weights, 
+       w = w, df.residual = resdf, df.null = nulldf, y = y, converged = conv, 
+       boundary = boundary, Amat = Amat, bvec = bvec, meq = meq, iact = fit$iact)
+}

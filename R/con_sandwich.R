46232de8963d2d5ffccf01e69fba608cda889daf
LeonardV <lgf.vanbrabant@gmail.com>
2017-01-05 12:13:06 +0100

    add bread and estfun for glm

--- R/con_sandwich.R
+++ R/con_sandwich.R
@@ -9,27 +9,14 @@ sandwich <- function(x, bread. = bread, meat. = meatHC, ...) {
 }
 
 
-# meat <- function(x, adjust = FALSE, ...)
-# {
-#   if (is.list(x) && !is.null(x$na.action)) class(x$na.action) <- "omit"
-#   psi <- estfun(x, ...)
-#   k <- NCOL(psi)
-#   n <- NROW(psi)
-#   rval <- crossprod(as.matrix(psi))/n
-#   if (adjust) rval <- n/(n-k) * rval
-#   rownames(rval) <- colnames(rval) <- colnames(psi)
-#   return(rval)
-# }
-
-
 bread <- function(x, ...) {
   UseMethod("bread")
 }
 
 bread.conLM <- function(x, ...) {
-  I.inv <- attr(x$information, "inverted")
-  cov.unscaled <- 1/x$s2.restr * I.inv
-  return(cov.unscaled * as.vector(sum(summary(x$model.org)$df[1:2])))
+  I_inv <- attr(x$information, "inverted")
+  cov.unscaled <- 1/x$s2_restr * I_inv
+  return(cov.unscaled * as.vector(sum(summary(x$model_org)$df[1:2])))
 }
 
 
@@ -38,10 +25,10 @@ bread.conRLM <- function(x, ...) {
     wts <- weights(x)
     if (is.null(wts)) { wts <- 1 }
     res <- residuals(x)
-    psi_deriv <- function(z) x$model.org$psi(z, deriv = 1)
-    rval <- sqrt(abs(as.vector(psi_deriv(res / x$model.org$s) / x$model.org$s))) * wts * xmat    
+    psi_deriv <- function(z) x$model_org$psi(z, deriv = 1)
+    rval <- sqrt(abs(as.vector(psi_deriv(res / x$model_org$s) / x$model_org$s))) * wts * xmat    
     rval <- chol2inv(qr.R(qr(rval))) * nrow(xmat)
-    rval <- solve(rval) #/ x$model.org$s * nrow(xmat)                           #solve(rval)
+    rval <- solve(rval) #/ x$model_org$s * nrow(xmat)                           #solve(rval)
     
     is.augmented <- TRUE
     if (all(c(x$constraints) == 0)) { is.augmented <- FALSE }
@@ -49,14 +36,29 @@ bread.conRLM <- function(x, ...) {
     rval <- con_augmented_information(information  = rval, 
                                       is.augmented = is.augmented,
                                       X            = xmat, 
-                                      b.unrestr    = x$b.unrestr, 
-                                      b.restr      = x$b.restr, 
+                                      b_unrestr    = x$b_unrestr, 
+                                      b_restr      = x$b_restr, 
                                       Amat         = x$constraints, 
                                       bvec         = x$rhs, 
                                       meq          = x$neq)
     return(rval$information)
 }
 
+
+bread.conGLM <- function(x, ...) {
+  sx <- summary(x$model_org)
+  wres <- as.vector(residuals(x, "working")) * weights(x, "working")
+  dispersion_restr <- if (substr(x$model_org$family$family, 1, 17) %in% c("poisson", "binomial", "Negative Binomial")) {
+    1
+  } else {
+    sum(wres^2) / sum(weights(x, "working"))
+  }
+  
+  I_inv <- (attr(x$information, "inverted") * x$df.residual) / as.vector(sum(sx$df[1:2]))
+  cov.unscaled <- 1 / dispersion_restr * I_inv
+  
+  return(cov.unscaled * as.vector(sum(sx$df[1:2])) * dispersion_restr)
+}
 
 
 estfun <- function(x, ...) {
@@ -75,12 +77,7 @@ estfun.conLM <- function(x, ...) {
   rval <- as.vector(res) * wts * xmat
   attr(rval, "assign") <- NULL
   attr(rval, "contrasts") <- NULL
-#  if (is.zoo(res)) {
-#    rval <- zoo(rval, index(res), attr(res, "frequency"))
-#  }
-#  if (is.ts(res)) {
-#    rval <- ts(rval, start = start(res), frequency = frequency(res))
-#  }
+
   return(rval)
 }
 
@@ -89,17 +86,35 @@ estfun.conRLM <- function(x, ...) {
   wts <- weights(x)
   if (is.null(wts)) { wts <- 1 }
   res <- residuals(x)
-  psi <- function(z) x$model.org$psi(z) * z
-  rval <- as.vector(psi(res/x$model.org$s)) * wts * xmat
+  psi <- function(z) x$model_org$psi(z) * z
+  rval <- as.vector(psi(res/x$model_org$s)) * wts * xmat
   attr(rval, "assign") <- NULL
   attr(rval, "contrasts") <- NULL
-#  if (is.ts(res)) {
-#    rval <- ts(rval, start = start(res), frequency = frequency(res))
-#  }
-#  if (is.zoo(res)) {
-#    rval <- zoo(rval, index(res), attr(res, "frequency"))
-#  }
+
+  return(rval)
+}
+
+
+estfun.conGLM <- function(x, ...) {
+  xmat <- model.matrix(x)
+  if (any(alias <- is.na(coef(x)))) {
+    xmat <- xmat[, !alias, drop = FALSE]
+  }
+  wres <- as.vector(residuals(x, "working")) * weights(x, "working")
+  #dispersion <- x$dispersion                                    # or x$dispersion_restr
   
+  dispersion <- if (substr(x$model_org$family$family, 1, 17) %in% c("poisson", "binomial", "Negative Binomial")) {
+    1
+  } else {
+    sum(wres^2, na.rm = TRUE) / sum(weights(x, "working"), na.rm = TRUE)
+  }
+  
+  rval <- wres * xmat / dispersion
+  attr(rval, "assign") <- NULL
+  attr(rval, "contrasts") <- NULL
+#  res <- residuals(x, type = "pearson")
+#  if(is.ts(res)) rval <- ts(rval, start = start(res), frequency = frequency(res))
+#  if(is.zoo(res)) rval <- zoo(rval, index(res), attr(res, "frequency"))
   return(rval)
 }
 
@@ -108,53 +123,54 @@ meatHC <- function(x,
                    type = c("HC3", "const", "HC", "HC0", "HC1", "HC2", "HC4", "HC4m", "HC5"),
                    omega = NULL) {
   
-  X <- model.matrix(x$model.org)
+  X <- model.matrix(x$model_org)
   if (any(alias <- is.na(coef(x)))) X <- X[, !alias, drop = FALSE]
   attr(X, "assign") <- NULL
   n <- NROW(X)
   
   ### get hat values and residual degrees of freedom ###
-  if (inherits(x, "conRLM")) {
-    #W <- sqrt(x$model.org$w)                                     #constrained or unconstrained?
-    # sqrt of the weights
-    rW <- sqrt(x$w)
-  } else if (inherits(x, "conLM")) {
-      if (!is.null(x$weights)) {
-        rW <- sqrt(x$weights)
-      } else {
-        rW <- rep(1, n)
+  if (type %in% c("HC2", "HC3", "HC4", "HC4m", "HC5")) {
+    if (inherits(x, "conRLM")) {
+      rW <- sqrt(x$w)
+    } else if (inherits(x, "conLM")) {
+        if (!is.null(x$weights)) {
+          rW <- sqrt(x$weights)
+        } else {
+          rW <- rep(1, n)
+        }
+    } else if (inherits(x, "conGLM")) {
+      rW <- sqrt(x$weights)
       }
-  } 
-  
-  
-  ### compute hat matrix ###
-  ### code added by LV ###
-  
-  # in rare situations, it may happen that the weight equals 0.
-  # to avoid computational issues, we replace the zero with 1e-08
-  idx.rW <- which(rW == 0)
-  rW[idx.rW] <- 1e-08
-  
-  if (any(rW) == 0L) {
-    warning("Restriktor WARNING: weights ", idx.rW, " are exactly zero. This causes problems for", 
-            "\ncomputing the hat-matrix. To compute the ", sQuote(type), " standard errors, we added a small",
-            "\nnumber 1e-08. Note that the results may not be trustworthy.")
+    
+    
+    ### compute hat matrix ###
+    ### code added by LV ###
+    # it may happen that the weight equals 0.
+    # to avoid computational issues, we replace the zero with 1e-08
+    idx.rW <- which(rW == 0)
+    rW[idx.rW] <- 1e-08
+    
+    if (any(rW == 0)) {
+      warning("Restriktor WARNING: weights ", idx.rW, " are exactly zero. This causes problems for", 
+              "\ncomputing the hat-matrix. To compute the ", sQuote(type), " standard errors, we added a small",
+              "\nnumber 1e-08. Note that the results may not be trustworthy.")
+    }
+    
+    ## matrix form ##
+    # diaghat <- diag(X %*% solve(t(X) %*% W %*% X) %*% t(X) %*% W)
+    
+    # QR factorization #
+    # rescaled X
+    rWX <- rW * X
+    # QR factorization
+    QR <- qr.default(rWX)
+    Q <- qr.qy(QR, diag(1, nrow = nrow(QR$qr), ncol = QR$rank))
+    # for weighted least squares
+    Q1 <- (1 / rW) * Q
+    Q2 <- rW * Q
+    ## diagonal
+    diaghat <- rowSums(Q1 * Q2)  
   }
-  
-  ## matrix form ##
-  # diaghat <- diag(X %*% solve(t(X) %*% W %*% X) %*% t(X) %*% W)
-  
-  # QR factorization #
-  # rescaled X
-  rWX <- rW * X
-  # QR factorization
-  QR <- qr.default(rWX)
-  Q <- qr.qy(QR, diag(1, nrow = nrow(QR$qr), ncol = QR$rank))
-  # for weighted least squares
-  Q1 <- (1 / rW) * Q
-  Q2 <- rW * Q
-  ## diagonal
-  diaghat <- rowSums(Q1 * Q2)  
   
   
   p <- NCOL(X)
@@ -204,7 +220,7 @@ meatHC <- function(x,
   ## process omega
   if (is.function(omega)) { omega <- omega(res, diaghat, df) }
   rval <- sqrt(omega) * X
-  rval <- crossprod(rval)/n
+  rval <- crossprod(rval) / n
   
   return(rval)
 }

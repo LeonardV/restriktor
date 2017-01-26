10d8e946e3d86000fb21952cf504867dbc9af475
LeonardV <lgf.vanbrabant@gmail.com>
2017-01-26 17:06:50 +0100

    updated for glm

--- R/con_boot.R
+++ R/con_boot.R
@@ -3,18 +3,19 @@ con_boot_lm <- function(B = 999, fixed = FALSE, ...) {
   ldots <- list(...)
   z <- ldots$object
   DATA <- as.data.frame(z$model)
-  if (!fixed) { # standard bootstrap
+  if (!fixed) { 
+    # standard bootstrap
     bootout <- boot(DATA,  
                     con_boot_data, 
                     R = B, ...)
-  } else { # model based bootstrap
+  } else { 
+    # model based bootstrap
     res <- z$residuals
     fit <- z$fitted.values
     bootout <- boot(data.frame(DATA, fit = fit, res = res), 
                     con_boot_fixed, R = B, ...)
   }
   
-  OUT <- bootout
+  bootout
   
-  OUT
 }

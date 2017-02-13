2680b953d6686681786f12ca308f6251a546558d
LeonardV <lgf.vanbrabant@gmail.com>
2017-02-13 15:51:09 +0100

    add version

--- R/restriktor_print.R
+++ R/restriktor_print.R
@@ -4,12 +4,25 @@ print.restriktor <- function(x, digits = max(3, getOption("digits") - 2), ...) {
   cat("\nCall:\n", gsub("\\n", "\n", paste(deparse(x$call), sep = "\n", collapse="\n"), 
                         fixed = TRUE), "\n\n", sep = "")
   
+  
+  cat(sprintf("restriktor (%s): ", packageDescription("restriktor", fields = "Version")))
+  
   if (inherits(x, "conRLM")) {
-    cat("Restriktor: restricted robust linear model:\n\n")
+    cat("restricted robust linear model:\n")
+    if (x$converged) {
+      cat("Converged in", length(x$iter), "iterations\n\n")
+    } else {
+      cat("Ran", length(x$iter), "iterations without convergence\n\n")
+    }
   } else if (inherits(x,"conGLM")) {
-    cat("Restriktor: restricted generalized linear model:\n\n")
+    cat("restricted generalized linear model:\n")
+    if (x$converged) {
+      cat("Converged in", x$iter, "iterations\n\n")
+    } else {
+      cat("Ran", x$iter, "iterations without convergence\n\n")
+    }
   } else if (inherits(x,"conLM")) {
-    cat("Restriktor: restricted linear model:\n\n")
+    cat("restricted linear model:\n\n")
   }
   
   if (length(coef(x)) > 0L) {

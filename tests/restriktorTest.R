b82dc1caeb8f68034d16e613a218311433ef1ff4
LeonardV <lgf.vanbrabant@gmail.com>
2017-02-15 19:03:11 +0100

    update

--- tests/restriktorTest.R
+++ tests/restriktorTest.R
@@ -13,42 +13,42 @@ myConstraints <- ' Group1 < Group2
 fit_lm <- lm(Age ~ -1 + Group, data = DATA)
 
 fit1a_con <- restriktor(fit_lm, constraints = myConstraints,
-                       Wt = "pmvnorm", bootWt.R = 999,
+                       mix.weights = "pmvnorm", mix.bootstrap = 999,
                        se = "boot.model.based", B = 99)
 summary(fit1a_con)
 conTest(fit1a_con)
 
 
 fit1b_con <- restriktor(fit_lm, constraints = myConstraints,
-                       Wt = "boot", bootWt.R = 999,
+                       mix.weights = "boot", mix.bootstrap = 999,
                        se = "boot.model.based", B = 99)
 summary(fit1b_con)
 conTest(fit1b_con)
 
 
 fit1c_con <- restriktor(fit_lm, constraints = myConstraints,
-                       Wt = "none", bootWt.R = 999,
+                       mix.weights = "none", mix.bootstrap = 999,
                        se = "boot.model.based", B = 99)
 summary(fit1c_con)
 conTest(fit1c_con)
 
 
 fit2a_con <- restriktor(fit_lm, constraints = myConstraints,
-                       Wt = "pmvnorm", bootWt.R = 999,
+                       mix.weights = "pmvnorm", mix.bootstrap = 999,
                        se = "boot.standard", B = 99)
 
 summary(fit2a_con)
 conTest(fit2a_con)
 
 fit2b_con <- restriktor(fit_lm, constraints = myConstraints,
-                       Wt = "boot", bootWt.R = 999,
+                       mix.weights = "boot", mix.bootstrap = 999,
                        se = "boot.standard", B = 99)
 
 summary(fit2b_con)
 conTest(fit2b_con)
 
 fit2c_con <- restriktor(fit_lm, constraints = myConstraints,
-                       Wt = "none", bootWt.R = 999,
+                       mix.weights = "none", mix.bootstrap = 999,
                        se = "boot.standard", B = 99)
 
 summary(fit2c_con)
@@ -56,21 +56,21 @@ conTest(fit2c_con)
 
 
 fit3a_con <- restriktor(fit_lm, constraints = myConstraints,
-                        Wt = "none", bootWt.R = 999,
+                        mix.weights = "none", mix.bootstrap = 999,
                         se = "boot.standard", B = 99)
 summary(fit3a_con)
 conTest(fit3a_con)
 
 
 fit3b_con <- restriktor(fit_lm, constraints = myConstraints,
-                        Wt = "none", bootWt.R = 999,
+                        mix.weights = "none", mix.bootstrap = 999,
                         se = "boot.standard", B = 99)
 summary(fit3b_con)
 conTest(fit3b_con)
 
 
 fit3c_con <- restriktor(fit_lm, constraints = myConstraints,
-                        Wt = "none", bootWt.R = 999,
+                        mix.weights = "none", mix.bootstrap = 999,
                         se = "boot.standard", B = 99)
 summary(fit3c_con)
 conTest(fit3c_con)
@@ -80,42 +80,42 @@ library(MASS)
 fit_rlm <- rlm(Age ~ -1 + Group, data = DATA, method = "MM", maxit = 5000)
 
 fit1a_con <- restriktor(fit_rlm, constraints = myConstraints,
-                        Wt = "pmvnorm", bootWt.R = 999,
+                        mix.weights = "pmvnorm", mix.bootstrap = 999,
                         se = "boot.model.based", B = 99)
 summary(fit1a_con)
 conTest(fit1a_con)
 
 
 fit1b_con <- restriktor(fit_rlm, constraints = myConstraints,
-                        Wt = "boot", bootWt.R = 999,
+                        mix.weights = "boot", mix.bootstrap = 999,
                         se = "boot.model.based", B = 99)
 summary(fit1b_con)
 conTest(fit1b_con)
 
 
 fit1c_con <- restriktor(fit_rlm, constraints = myConstraints,
-                        Wt = "none", bootWt.R = 999,
+                        mix.weights = "none", mix.bootstrap = 999,
                         se = "boot.model.based", B = 99)
 summary(fit1c_con)
 conTest(fit1c_con)
 
 
 fit2a_con <- restriktor(fit_rlm, constraints = myConstraints,
-                        Wt = "pmvnorm", bootWt.R = 999,
+                        mix.weights = "pmvnorm", mix.bootstrap = 999,
                         se = "boot.standard", B = 99)
 
 summary(fit2a_con)
 conTest(fit2a_con)
 
 fit2b_con <- restriktor(fit_rlm, constraints = myConstraints,
-                        Wt = "boot", bootWt.R = 999,
+                        mix.weights = "boot", mix.bootstrap = 999,
                         se = "boot.standard", B = 99)
 
 summary(fit2b_con)
 conTest(fit2b_con)
 
 fit2c_con <- restriktor(fit_rlm, constraints = myConstraints,
-                       Wt = "none", bootWt.R = 999,
+                       mix.weights = "none", mix.bootstrap = 999,
                        se = "boot.standard", B = 99)
 
 summary(fit2c_con)
@@ -123,21 +123,21 @@ conTest(fit2c_con)
 
 
 fit3a_con <- restriktor(fit_rlm, constraints = myConstraints,
-                        Wt = "none", bootWt.R = 999,
+                        mix.weights = "none", mix.bootstrap = 999,
                         se = "boot.standard", B = 99)
 summary(fit3a_con)
 conTest(fit3a_con)
 
 
 fit3b_con <- restriktor(fit_rlm, constraints = myConstraints,
-                        Wt = "none", bootWt.R = 999,
+                        mix.weights = "none", mix.bootstrap = 999,
                         se = "boot.standard", B = 99)
 summary(fit3b_con)
 conTest(fit3b_con)
 
 
 fit3c_con <- restriktor(fit_rlm, constraints = myConstraints,
-                        Wt = "none", bootWt.R = 999,
+                        mix.weights = "none", mix.bootstrap = 999,
                         se = "boot.standard", B = 99)
 summary(fit3c_con)
 conTest(fit3c_con)
@@ -148,42 +148,42 @@ library(MASS)
 fit_glm <- glm(Age ~ -1 + Group, data = DATA, family = gaussian)
 
 fit1a_con <- restriktor(fit_glm, constraints = myConstraints,
-                        Wt = "pmvnorm", bootWt.R = 999,
+                        mix.weights = "pmvnorm", mix.bootstrap = 999,
                         se = "boot.model.based", B = 99)
 summary(fit1a_con)
 conTest(fit1a_con)
 
 
 fit1b_con <- restriktor(fit_glm, constraints = myConstraints,
-                        Wt = "boot", bootWt.R = 999,
+                        mix.weights = "boot", mix.bootstrap = 999,
                         se = "boot.model.based", B = 99)
 summary(fit1b_con)
 conTest(fit1b_con)
 
 
 fit1c_con <- restriktor(fit_glm, constraints = myConstraints,
-                        Wt = "none", bootWt.R = 999,
+                        mix.weights = "none", mix.bootstrap = 999,
                         se = "boot.model.based", B = 99)
 summary(fit1c_con)
 conTest(fit1c_con)
 
 
 fit2a_con <- restriktor(fit_glm, constraints = myConstraints,
-                        Wt = "pmvnorm", bootWt.R = 999,
+                        mix.weights = "pmvnorm", mix.bootstrap = 999,
                         se = "boot.standard", B = 99)
 
 summary(fit2a_con)
 conTest(fit2a_con)
 
 fit2b_con <- restriktor(fit_glm, constraints = myConstraints,
-                        Wt = "boot", bootWt.R = 999,
+                        mix.weights = "boot", mix.bootstrap = 999,
                         se = "boot.standard", B = 99)
 
 summary(fit2b_con)
 conTest(fit2b_con)
 
 fit2c_con <- restriktor(fit_glm, constraints = myConstraints,
-                        Wt = "none", bootWt.R = 999,
+                        mix.weights = "none", mix.bootstrap = 999,
                         se = "boot.standard", B = 99)
 
 summary(fit2c_con)
@@ -191,21 +191,21 @@ conTest(fit2c_con)
 
 
 fit3a_con <- restriktor(fit_glm, constraints = myConstraints,
-                        Wt = "none", bootWt.R = 999,
+                        mix.weights = "none", mix.bootstrap = 999,
                         se = "boot.standard", B = 99)
 summary(fit3a_con)
 conTest(fit3a_con)
 
 
 fit3b_con <- restriktor(fit_glm, constraints = myConstraints,
-                        Wt = "none", bootWt.R = 999,
+                        mix.weights = "none", mix.bootstrap = 999,
                         se = "boot.standard", B = 99)
 summary(fit3b_con)
 conTest(fit3b_con)
 
 
 fit3c_con <- restriktor(fit_glm, constraints = myConstraints,
-                        Wt = "none", bootWt.R = 999,
+                        mix.weights = "none", mix.bootstrap = 999,
                         se = "boot.standard", B = 99)
 summary(fit3c_con)
 conTest(fit3c_con)

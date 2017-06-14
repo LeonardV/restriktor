7813f6ec55a61a308c19c1da7d11b20bdc7948dc
LeonardV <lgf.vanbrabant@gmail.com>
2017-06-14 13:12:25 +0200

    remove pchibar

--- R/con_pbar.R
+++ R/con_pbar.R
@@ -12,15 +12,15 @@ pfbar <- function(x, df1, df2, wt.bar) {
 }
   
 
-pchibar <- function (x, df1, wt.bar) {
-  if (x <= 0) {
-    return(0)
-  }
-  zed <- df1 == 0
-  cdf <- ifelse(any(zed), wt.bar[zed], 0)
-  cdf <- cdf + sum(pchisq(x, df1[!zed]) * wt.bar[!zed])
-  return(cdf)
-}
+# pchibar <- function (x, df1, wt.bar) {
+#   if (x <= 0) {
+#     return(0)
+#   }
+#   zed <- df1 == 0
+#   cdf <- ifelse(any(zed), wt.bar[zed], 0)
+#   cdf <- cdf + sum(pchisq(x, df1[!zed]) * wt.bar[!zed])
+#   return(cdf)
+# }
 
   
   

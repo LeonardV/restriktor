d177b39aa3d16e25f4306d46f09c7e9a44d2f5e4
LeonardV <lgf.vanbrabant@gmail.com>
2017-06-28 09:37:02 +0200

    update tests

--- tests/testCases.R
+++ tests/testCases.R
@@ -38,42 +38,5 @@ constraints1 <- 'x1 > 0; x2 > 0; x3 > 0'
 fit.restr1 <- restriktor(linmod1, constraints1)
 fit.restr2 <- restriktor(linmod2, constraints1)
 fit.restr3 <- restriktor(linmod3, constraints1)
-#summary(fit.restr1)
 
-#iht
-ht1 <- iht(fit.restr1)
-round(ht1$global$pvalue[1], 4) == 1e-04
-round(ht1$A$pvalue[1], 4) == 0 
-round(ht1$B$pvalue[1], 4) == 0.7695
-round(ht1$C$pvalue[1], 4) == 0.6213
-
-## goric
-gw1 <- goric(fit.restr1, complement = TRUE)
-round(gw1$goric.weights[1], 4) == 0.7591
-round(gw1$goric.weights[2], 4) == 0.2409
-
-##
-#iht
-ht2 <- iht(fit.restr2)
-round(ht2$global$pvalue[1], 4) == 5e-04
-round(ht2$A$pvalue[1], 4) == 2e-04 
-round(ht2$B$pvalue[1], 4) == 1
-round(ht2$C$pvalue[1], 4) == 0.4261
-
-## goric
-gw2 <- goric(fit.restr2, complement = TRUE)
-round(gw2$goric.weights[1], 4) == 0.7568
-round(gw2$goric.weights[2], 4) == 0.2432
-
-##
-#iht
-ht3 <- iht(fit.restr3)
-round(ht3$global$pvalue[1], 4) == 4e-04
-round(ht3$A$pvalue[1], 4) == 2e-04
-round(ht3$B$pvalue[1], 4) == 0.5397
-round(ht3$C$pvalue[1], 4) == 0.7976
-
-## goric
-gw3 <- goric(fit.restr3, complement = TRUE)
-round(gw3$goric.weights[1], 4) == 0.7591
-round(gw3$goric.weights[2], 4) == 0.2409
+## to do: add tests

38026153771b85e6c55bba213a698cdc66741d30
LeonardV <lgf.vanbrabant@gmail.com>
2019-09-27 23:35:53 +0200

    add small sample correction for goric(a)

--- R/con_gorica_est_summary.R
+++ R/con_gorica_est_summary.R
@@ -1,38 +1,53 @@
-summary.gorica_est <- function(object, ...) {
+summary.gorica_est <- function(object, type, ...) {
   z <- object
+  
+  ldots <- list(...)
   
   if (!inherits(z, "gorica_est")) {
     stop("object of class ", sQuote(class(z)), " is not supported.")
   }
   
-  Amat <- z$constraints
-  meq <- z$neq
-  b.restr <- z$b.restr
+  Amat    <- z$constraints
+  meq     <- z$neq
+  wt.bar  <- z$wt.bar
   
-  ans <- z$model.org
-  
-  wt.bar <- z$wt.bar
+  ans     <- z$model.org
   ## compute goric
-  if (!(attr(wt.bar, "method") == "none")) {
+  if (!(attr(wt.bar, "method") == "none")) {  
     ## REF: Kuiper, R.M.; Hoijtink, H.J.A.; Silvapulle, M. J. (2012) 
     ## Journal of statistical planning and inference, volume 142, pp. 2454 - 2463
-    
-    # compute penalty term based on simulated level probabilities (wt.bar)
-    if (all(c(Amat) == 0)) {
-      # unconstrained case
-      PT <- length(b.restr)
-    } else if (attr(wt.bar, "method") == "boot") { 
-      PT <- sum(0 : ncol(Amat) * wt.bar)  
-    } else if (attr(wt.bar, "method") == "pmvnorm") {
-      min.C <- ncol(Amat) - nrow(Amat)
-      max.C <- ncol(Amat) - meq
-      PT <- sum(min.C : max.C * wt.bar) 
+    if (type %in% c("goric", "gorica")) {
+      PT <- penalty_goric(Amat        = Amat, 
+                          meq         = meq, 
+                          LP          = wt.bar, 
+                          correction  = FALSE, 
+                          sample.nobs = NULL)
+    } else if (type %in% c("goricc", "goricca")) {
+      PT <- penalty_goric(Amat        = Amat, 
+                          meq         = meq, 
+                          LP          = wt.bar, 
+                          correction  = TRUE, 
+                          sample.nobs = ldots$sample.nobs)
     } else {
-      stop("restriktor ERROR: unable to compute penalty for GORIC.")  
+      stop("restriktor ERROR: unable to compute penalty term value.")  
     }
+    
+    
+    # # compute penalty term based on simulated level probabilities (wt.bar)
+    # if (all(c(Amat) == 0)) {
+    #   # unconstrained case
+    #   PT <- length(b.restr)
+    # } else if (attr(wt.bar, "method") == "boot") { 
+    #   PT <- sum(0 : ncol(Amat) * wt.bar)  
+    # } else if (attr(wt.bar, "method") == "pmvnorm") {
+    #   min.C <- ncol(Amat) - nrow(Amat)
+    #   max.C <- ncol(Amat) - meq
+    #   PT <- sum(min.C : max.C * wt.bar) 
+    # } 
     
     ans$goric <- -2*(z$loglik - PT)
     attr(ans$goric, "penalty") <- PT
+    attr(ans$goric, "type") <- type
     attr(ans$goric, "loglik")  <- z$loglik 
   }
   

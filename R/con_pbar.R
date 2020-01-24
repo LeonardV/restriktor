81d019bb47988d4ce3202b9dd4059906b644aa45
LeonardV <lgf.vanbrabant@gmail.com>
2020-01-24 09:13:22 +0100

    code clean-up

--- R/con_pbar.R
+++ R/con_pbar.R
@@ -1,6 +1,3 @@
-## acknowlegment: taken from package ic.infer
-## pfbar added by LV.
-
 pfbar <- function(x, df1, df2, wt.bar) {
   if (x <= 0) {
     return(0)
@@ -12,15 +9,6 @@ pfbar <- function(x, df1, df2, wt.bar) {
 }
   
 
-# pchibar <- function (x, df1, wt.bar) {
-#   if (x <= 0) {
-#     return(0)
-#   }
-#   zed <- df1 == 0
-#   cdf <- ifelse(any(zed), wt.bar[zed], 0)
-#   cdf <- cdf + sum(pchisq(x, df1[!zed]) * wt.bar[!zed])
-#   return(cdf)
-# }
 
   
   

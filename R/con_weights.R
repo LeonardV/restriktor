db09e30e47a5da6e03b793098c20a117188e0167
LeonardV <lgf.vanbrabant@gmail.com>
2020-02-04 12:59:35 +0100

    remove parallel option from the con_weights_boot function. The function has been vectorized and much faster now. Parallel processing has become redundant.

--- R/con_weights.R
+++ R/con_weights.R
@@ -16,93 +16,44 @@ con_weights <- function(cov, meq) {
 ## REF: Silvapulle and Sen (2005, p. 79). Constrained Statistical Inference: Order, 
 ## Inequality, and Shape Constraints. Hoboken, {NJ}: Wiley
 con_weights_boot <- function(VCOV, Amat, meq, 
-                             R = 99999, parallel = c("no", "multicore", "snow"),
-                             ncpus = 1L, cl = NULL, seed = NULL, 
-                             verbose = FALSE, ...) {
+                             R = 99999L, seed = NULL, verbose = FALSE, ...) {
   
+  ## seed
+  if (!is.null(seed))
+    set.seed(seed)
+  if (!exists(".Random.seed", envir = .GlobalEnv)) 
+    runif(1)
+  RNGstate <- .Random.seed
   
-  parallel <- match.arg(parallel)
-  have_mc <- have_snow <- FALSE
-  if (parallel != "no" && ncpus > 1L) {
-    if (parallel == "multicore")
-      have_mc <- .Platform$OS.type != "windows"
-    else if (parallel == "snow")
-      have_snow <- TRUE
-    if (!have_mc && !have_snow)
-      ncpus <- 1L
-  }
-  
-  bvec <- rep(0L, nrow(Amat)) # weights do not depend on bvec.
+  ## weights do not depend on bvec.
+  bvec <- rep(0L, nrow(Amat)) 
   invW <- solve(VCOV) 
   Dmat <- 2*invW
-  
-  iact <- vector("numeric", ncol(Amat))
-  fn <- function(b) {
-    if (verbose) {
-      cat("bootstrap draw =", b)
-    }
-    if (!is.null(seed))
-      set.seed(seed + b)
-    if (!exists(".Random.seed", envir = .GlobalEnv))
-      runif(1)
-    RNGstate <- .Random.seed
-    
-    Z <- rmvnorm(n = 1, mean = rep(0, ncol(VCOV)), sigma = VCOV)
-    dvec <- 2*(Z %*% invW)
+  Z <- rmvnorm(n = R, mean = rep(0, ncol(VCOV)), sigma = VCOV)
+  dvec <- 2*(Z %*% invW)  
+  ## create vector
+  iact <- vector("numeric", R)
+  for (i in 1:R) {
     QP <- try(solve.QP(Dmat = Dmat, 
-                       dvec = dvec, 
+                       dvec = dvec[i, ], 
                        Amat = t(Amat),
                        bvec = bvec, 
                        meq  = meq))
     if (verbose) {
-      cat(" ...active inequality constraints =", QP$iact, "\n")
+      cat(" ...number of active constraints =", length(QP$iact), "\n")
     }
     
     if (inherits(QP, "try-error")) {
-      if (verbose) cat("quadprog FAILED\n")
-      return(NULL)
-    }    
-    if (QP$iact[1] == 0L) {
-      return(0L) 
-    } else { 
-      return(length(QP$iact))
-    }  
-  }
-  
-  RR <- sum(R)
-  res <- if (ncpus > 1L && (have_mc || have_snow)) {
-    if (have_mc) {
-      parallel::mclapply(seq_len(RR), fn, mc.cores = ncpus)
-    }
-    else if (have_snow) {
-      if (is.null(cl)) {
-        cl <- parallel::makePSOCKcluster(rep("localhost",
-                                             ncpus))
-        if (RNGkind()[1L] == "L'Ecuyer-CMRG")
-          parallel::clusterSetRNGStream(cl)
-        res <- parallel::parLapply(cl, seq_len(RR), fn)
-        parallel::stopCluster(cl)
-        res
+      if (verbose) {
+        cat("quadprog FAILED\n")
       }
-      else parallel::parLapply(cl, seq_len(RR), fn)
-    }
+      iact[i] <- as.numeric(NA)
+    }    
+    iact[i] <- ifelse(QP$iact[1] == 0L, 0L, length(QP$iact)) 
   }
-  else lapply(seq_len(RR), fn)
-  error.idx <- integer(0)
-  for (b in seq_len(R)) {
-    if (!is.null(res[[b]])) {
-      iact[b] <- res[[b]]
-    }
-    else {
-      error.idx <- c(error.idx, b)
-    }
-  }
-  # compute the number of positive components of VCOV.
-  # ncol(VCOV) = maximum number of constraints
-  # iact    = number of active inequality constraints
-  dimL <- ncol(VCOV) - iact
-  wt.bar <- sapply(1:(ncol(VCOV) + 1), function(x) sum(x == (dimL + 1))) / R
-  
+  dimL   <- ncol(VCOV) - iact
+  wt.bar <- sapply(1:ncol(VCOV), function(x) sum(x == dimL)) / R
+   
   wt.bar
 }
 

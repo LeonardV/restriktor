63348c4de964220c14bd0c5937cece8aeb6d8b7b
LeonardV <lgf.vanbrabant@gmail.com>
2020-02-12 21:05:47 +0100

    Fix issue if level probabilities are computed using simulation

--- R/con_goric_penalty.R
+++ R/con_goric_penalty.R
@@ -9,27 +9,27 @@ penalty_goric <- function(Amat, meq, LP, correction = FALSE,
       lPT <- ncol(Amat)
       PT  <- ( (N * (lPT + 1) / (N - lPT - 2)) ) 
     } else {
-    #} else if (attr(LP, "method") == "boot") { 
-      # if level-probabilities are simulated  
-      #lPT <- 1 : ncol(Amat)
-      #PT  <- sum( ( (N * (lPT + 1) / (N - lPT - 2) ) ) * LP)
-    # if level probabilities are computed using the multivariate normal distribution  
-    #} else if (attr(LP, "method") == "pmvnorm") {
-      min.col <- ncol(Amat) - nrow(Amat) # p - q1 - q2
-      max.col <- ncol(Amat) - meq        # p - q2
-      lPT     <- min.col : max.col
-      PT      <- sum( ( (N * (lPT + 1) / (N - lPT - 2) ) ) * LP) 
+      if (attr(LP, "method") == "boot") { 
+        lPT <- 0 : ncol(Amat)
+        PT  <- sum( ( (N * (lPT + 1) / (N - lPT - 2) ) ) * LP)
+      } else if (attr(LP, "method") == "pmvnorm") {
+        min.col <- ncol(Amat) - nrow(Amat) # p - q1 - q2
+        max.col <- ncol(Amat) - meq        # p - q2
+        lPT     <- min.col : max.col
+        PT      <- sum( ( (N * (lPT + 1) / (N - lPT - 2) ) ) * LP) 
+      }
     }
   } else {
     if (all(c(Amat) == 0)) {
       PT <- 1 + ncol(Amat)
     } else {
-    # } else if (attr(LP, "method") == "boot") {  
-    #   PT <- 1 + sum(1 : ncol(Amat) * LP)  
-    # } else if (attr(LP, "method") == "pmvnorm") {
-      min.col <- ncol(Amat) - nrow(Amat) # p - q1 - q2
-      max.col <- ncol(Amat) - meq        # p - q2
-      PT <- 1 + sum(min.col : max.col * LP) 
+      if (attr(LP, "method") == "boot") {  
+        PT <- 1 + sum(0 : ncol(Amat) * LP)  
+      } else if (attr(LP, "method") == "pmvnorm") {
+        min.col <- ncol(Amat) - nrow(Amat) # p - q1 - q2
+        max.col <- ncol(Amat) - meq        # p - q2
+        PT <- 1 + sum(min.col : max.col * LP) 
+      }
     }
   }
   

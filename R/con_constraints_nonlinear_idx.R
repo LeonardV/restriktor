96cccc7a13f08ac82cff75e5cb4623a385add3b2
LeonardV <lgf.vanbrabant@gmail.com>
2021-03-24 10:38:17 +0100

    add function for checking for nonlinear constraints. This is function is taken from the lavaan package.

---
+++ R/con_constraints_nonlinear_idx.R
@@ -0,0 +1,10 @@
+# this is originally a function from the lavaan package.
+con_constraints_nonlinear_idx <- function (func = NULL, npar = NULL) {
+  if (is.null(func) || is.null(body(func))) 
+    return(integer(0L))
+  A0 <- lav_func_jacobian_complex(func = func, x = rnorm(npar))
+  A1 <- lav_func_jacobian_complex(func = func, x = rnorm(npar))
+  A0minA1 <- A0 - A1
+  linear <- apply(A0minA1, 1, function(x) all(x == 0))
+  which(!linear)
+}

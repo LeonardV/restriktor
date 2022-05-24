3c3b3caa971401af9b288bd98ec5e6a737e037b3
LeonardV <lgf.vanbrabant@gmail.com>
2022-05-24 14:55:58 +0200

    prepare for auto_bound argument

--- R/restriktor.R
+++ R/restriktor.R
@@ -14,7 +14,7 @@ restriktor <- function(object, constraints = NULL, ...) {
   arguments <- list(...)
   if (length(arguments)) {
     pnames <- c("se", "B", "rhs", "neq", "mix.weights", "mix.bootstrap", "parallel", 
-                "ncpus", "cl", "seed", "control", "verbose", "debug")
+                "ncpus", "cl", "seed", "control", "verbose", "debug", "auto_bound")
     pm <- pmatch(names(arguments), pnames, nomatch = 0L)
     if (any(pm == 0L)) { 
       pm.idx <- which(pm == 0L)

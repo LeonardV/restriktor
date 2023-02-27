1480cb0983c3c91fae919e0a9b2993fac42039e2
LeonardV <lgf.vanbrabant@gmail.com>
2023-02-27 14:56:42 +0100

    code cleaning

--- R/restriktor.R
+++ R/restriktor.R
@@ -19,21 +19,20 @@ restriktor <- function(object, constraints = NULL, ...) {
     pm <- pmatch(names(arguments), pnames, nomatch = 0L)
     if (any(pm == 0L)) { 
       pm.idx <- which(pm == 0L)
-      stop("Restriktor ERROR: ", names(arguments[pm.idx]), " invalid argument(s).")
+      stop("Restriktor Error: ", names(arguments[pm.idx]), " invalid argument(s).")
     }
   }
 
   if (class(object)[1] == "lm") {
     conLM(object, constraints, ...)
-  } 
-  else if (class(object)[1] == "rlm") {
+  } else if (class(object)[1] == "rlm") {
     conRLM(object, constraints, ...)
-  }
-  else if (class(object)[1] == "glm") {
+  } else if (class(object)[1] == "glm") {
     conGLM(object, constraints, ...)
-  }
-  else if (class(object)[1] == "mlm") {
+  } else if (class(object)[1] == "mlm") {
     conMLM(object, constraints, ...)
-  }  
-
+  } else {
+    stop("Restriktor Error: I don't know how to handles objects of class", 
+         class(object), call. = FALSE)
+  } 
 }

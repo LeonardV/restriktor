e16c26d1397e08c349879b567fc9f96b3f5f2f73
LeonardV <lgf.vanbrabant@gmail.com>
2023-11-13 16:11:22 +0100

    add pchibar

--- R/con_pbar.R
+++ R/con_pbar.R
@@ -8,6 +8,15 @@ pfbar <- function(x, df1, df2, wt.bar) {
   return(cdf)
 }
   
+pchibar <- function(x, df, wt.bar) {
+  if (x <= 0) {
+    return(0)
+  }
+  zed <- df == 0
+  cdf <- ifelse(any(zed), wt.bar[zed], 0)
+  cdf <- cdf + sum(pchisq(x, df[!zed]) * wt.bar[!zed])
+  return(cdf)
+}
 
 
   

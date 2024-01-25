b238ea5a49c647ed59fced64ca3982f0a1903aa8
LeonardV <lgf.vanbrabant@gmail.com>
2024-01-25 17:31:12 +0100

    pass trmvnorm using ldots

--- R/con_gorica_est.R
+++ R/con_gorica_est.R
@@ -1,6 +1,6 @@
 con_gorica_est <- function(object, constraints = NULL, VCOV = NULL,
                            rhs = NULL, neq = 0L, mix_weights = "pmvnorm", 
-                           mix_weights_bootstrap_limit = 1e5L, parallel = "no", 
+                           mix_weights_bootstrap_limit = 1e5L, #parallel = "no", 
                            ncpus = 1L, cl = NULL, seed = NULL, 
                            control = list(), verbose = FALSE, debug = FALSE, ...) {
   
@@ -210,7 +210,8 @@ con_gorica_est <- function(object, constraints = NULL, VCOV = NULL,
                                                            1e-03, control$convergence_crit),
                                  chunk_size = ifelse(is.null(control$chunk_size), 
                                                            5000L, control$chunk_size),
-                                 verbose          = verbose)
+                                 verbose          = verbose, 
+                                 ...)
       attr(wt.bar, "mix_weights_bootstrap_limit") <- mix_weights_bootstrap_limit 
     } else if (mix_weights == "pmvnorm" && (meq < nrow(Amat))) {
       # compute chi-square-bar weights based on pmvnorm

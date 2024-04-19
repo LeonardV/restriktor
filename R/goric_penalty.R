a9c7c4191e14231819fd5132cc021d6a3e2df650
LeonardV <lgf.vanbrabant@gmail.com>
2024-04-19 16:42:04 +0200

    function to compute PT for the complement of Hm

--- R/goric_penalty.R
+++ R/goric_penalty.R
@@ -66,3 +66,50 @@ penalty_goric <- function(Amat, meq, LP, correction = FALSE,
 
   #return(PT)
 }
+
+
+penalty_complement_goric <- function(VCOV, Amat, meq, type, wt.bar, 
+                                     sample.nobs = NULL, debug = FALSE) {
+  # compute the number of free parameters f in the complement
+  p <- ncol(VCOV)
+  # rank q1
+  #lq1 <- qr(Amat.ciq)$rank
+  lq1 <- qr(Amat)$rank - meq
+  
+  # compute penalty term value PTc
+  if (type %in% c("goric", "gorica")) {
+    idx <- length(wt.bar)
+    if (attr(wt.bar, "method") == "boot") {
+      PTc <- as.numeric(1 + p - wt.bar[idx-meq] * lq1)  
+    } else if (attr(wt.bar, "method") == "pmvnorm") {
+      PTc <- as.numeric(1 + p - wt.bar[idx] * lq1) 
+    } else {
+      stop("restriktor ERROR: no level probabilities (chi-bar-square weights) found.")
+    }
+  } else if (type %in% c("goricc", "goricac")) {
+    idx <- length(wt.bar) 
+    if (is.null(sample.nobs)) {
+      stop("restriktor ERROR: the argument sample.nobs is not found.")
+    }
+    N <- sample.nobs
+    # small sample correction
+    if (attr(wt.bar, "method") == "boot") {
+      PTc <- 1 + wt.bar[idx-meq] * (N * (p - lq1) + (p - lq1) + 2) / (N - (p - lq1) - 2) + 
+        (1 - wt.bar[idx-meq]) * (N * p + p + 2) / (N - p - 2)
+    } else if (attr(wt.bar, "method") == "pmvnorm") {
+      PTc <- 1 + wt.bar[idx] * (N * (p - lq1) + (p - lq1) + 2) / (N - (p - lq1) - 2) + 
+        (1 - wt.bar[idx]) * (N * p + p + 2) / (N - p - 2) 
+    } 
+  }
+  
+  if (debug) {
+    cat("penalty term value =", PTc, "\n")
+  }
+  
+  # correction for gorica(c): -1 because no 
+  if (type %in% c("gorica", "goricac")) {
+    PTc <- PTc - 1
+  }
+  
+  return(PTc)
+}

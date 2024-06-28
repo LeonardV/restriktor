add4d0b66ffc05222203d45df7bd54f9734e7eba
LeonardV <lgf.vanbrabant@gmail.com>
2024-06-28 14:38:07 +0200

    rename function

---
+++ R/goric_benchmark_print.R
@@ -0,0 +1,120 @@
+print.benchmark <- function(x, 
+                            output_type = c("all", "gw", "rgw", "rlw", "ld"), 
+                            ...) {
+
+  # Ensure the object is of class 'benchmark_means'
+  if (!inherits(x, "benchmark")) {
+    stop("Invalid object. The object should be of class 'benchmark'.", call. = FALSE)
+  }
+  
+  model_type <- class(x)[1]
+  goric_type <- capitalize_first_letter(x$type)
+  pref_hypo <- x$pref_hypo_name
+  error_prob_pref_hypo <- x$error_prob_pref_hypo_name
+  error_prob_pref_hypo <- 
+    if (error_prob_pref_hypo < 0.001) {
+      "<.001"
+    } else {
+      sprintf("%.3f", error_prob_pref_hypo)
+    }
+  
+  # means specific
+  if (inherits(x, "benchmark_means")) {
+    formatted_values <- sprintf("%.3f", x$pop_es)
+    ratio_pop_means <- x$ratio_pop_means
+    group_size <- x$group_size
+    ngroups <- x$ngroups
+    cohens_f_observed <- x$cohens_f_observed
+  } else if (inherits(x, "benchmark_asymp")) {
+    pop_est <- x$pop_est
+    formatted_values <- sprintf("%.3f", pop_est)
+    dim(formatted_values) <- dim(pop_est)
+    dimnames(formatted_values) <- dimnames(pop_est)
+    group_size <- x$sample_size
+    ngroups <- x$n_coef
+  } 
+  
+  # ANSI escape codes for colors
+  blue <- "\033[34m"
+  green <- "\033[32m"
+  reset <- "\033[0m"
+  
+  text_gw  <- paste0("Benchmark Analysis: Percentiles of ", goric_type, "-Weights for the Preferred Hypothesis '", pref_hypo, "'")
+  text_rgw <- paste0("Benchmark Analysis: Percentiles of Ratios of ", goric_type, "-Weights for the Preferred Hypothesis '", pref_hypo, "'")
+  text_rlw <- paste0("Benchmark Analysis: Percentiles of Ratios of Likelihood Weights for the Preferred Hypothesis '", pref_hypo, "'")
+  text_ld  <- paste0("Benchmark Analysis: Percentiles of Differences in Likelihood Values for the Preferred Hypothesis '", pref_hypo, "'")
+  
+  cat("\n")
+  #cat(strrep("=", 70), "\n")
+  cat(paste0(blue, "Benchmark Results", reset), "\n")
+  cat(strrep("-", 70), "\n")
+  cat(sprintf("Preferred Hypothesis: %s%s%s\n", green, pref_hypo, reset))
+  cat(sprintf("Error probability Preferred Hypothesis: %s%s%s\n", green, error_prob_pref_hypo, reset))
+  if (inherits(x, "benchmark_means")) {
+    cat(sprintf("Number of Groups: %s%s%s\n", green, ngroups, reset))
+  } else {
+    cat(sprintf("Number of Parameters: %s%s%s\n", green, ngroups, reset))
+  }
+  if (inherits(x, "benchmark_means")) {
+    cat(sprintf("Group Size: %s%s%s\n", green, paste(group_size, collapse = ", "), reset))
+    cat(sprintf("Ratio of Population Means: %s%s%s\n", green, paste(sprintf("%.3f", ratio_pop_means), collapse = ", "), reset))
+    cat(sprintf("Population Effect-Sizes Cohens f: %s%s%s\n", green,  paste(formatted_values, collapse = ", "), reset))
+    cat(sprintf("Observed Effect-Size Cohens f: %s%s%s\n", green, sprintf("%.3f", cohens_f_observed), reset))
+  } else {
+    cat(sprintf("Sample Size: %s%s%s\n", green, paste(group_size, collapse = ", "), reset))
+    print_formatted_matrix(formatted_values, green, reset)
+  }
+  #cat(strrep("-", 70), "\n")
+  cat("\n")
+  
+  # Normalize output_type to lowercase
+  output_type <- tolower(output_type)
+  
+  # Print benchmarks for Goric-weights percentiles
+  if ("all" %in% output_type || "gw" %in% output_type) { 
+    print_section(
+      text_gw,
+      function() {
+        for (pop_es in names(x$benchmarks_goric_weights)) {
+          print_rounded_es_value(x$benchmarks_goric_weights[[pop_es]], pop_es, model_type, green, reset)
+        }
+      }, nchar(text_gw), text_color = blue, reset = reset
+    )
+  }
+  
+  # Print benchmarks for ratio Goric-weights percentiles
+  if ("all" %in% output_type || "rgw" %in% output_type) {
+    print_section(
+      text_rgw,
+      function() {
+        for (pop_es in names(x$benchmarks_ratio_goric_weights)) {
+          print_rounded_es_value(x$benchmarks_ratio_goric_weights[[pop_es]], pop_es, model_type, green, reset)
+        }
+      }, nchar(text_rgw), text_color = blue, reset = reset
+    )
+  }
+  
+  # Print benchmarks for ratio likelihood-weights percentiles
+  if ("all" %in% output_type || "rlw" %in% output_type) {
+    print_section(
+      text_rlw,
+      function() {
+        for (pop_es in names(x$benchmarks_ratio_ll_weights)) {
+          print_rounded_es_value(x$benchmarks_ratio_ll_weights[[pop_es]], pop_es, model_type, green, reset)
+        }
+      }, nchar(text_rlw), text_color = blue, reset = reset
+    )
+  }
+  
+  # Print benchmarks for difference likelihood-values percentiles
+  if ("all" %in% output_type || "ld" %in% output_type) {
+    print_section(
+      text_ld,
+      function() {
+        for (pop_es in names(x$benchmarks_difLL)) {
+          print_rounded_es_value(x$benchmarks_difLL[[pop_es]], pop_es, model_type, green, reset)
+        }
+      }, nchar(text_ld), text_color = blue, reset = reset
+    )
+  }
+}
---
+++ R/goric_benchmark_utilities.R
@@ -0,0 +1,279 @@
+# Compute Cohen's f based on group_means, N, and VCOV
+compute_cohens_f <- function(group_means, N, VCOV) {
+  total_mean <- sum(group_means * N) / sum(N) 
+  ss_between <- sum(N * (group_means - total_mean)^2) 
+  cov_matrix <- VCOV * N
+  ss_within <- sum((N - 1) * diag(cov_matrix)) 
+  cohens_f <- sqrt(ss_between/ss_within)
+  
+  return(cohens_f)
+}
+
+
+# Compute ratio data based on group_means
+compute_ratio_data <- function(group_means) {
+  ngroups <- length(group_means)
+  ratio_data <- rep(NA, ngroups)
+  ratio_data[order(group_means) == 1] <- 1
+  ratio_data[order(group_means) == 2] <- 2
+  # The choice of the smallest and the second smallest mean makes the scaling 
+  # more robust against changes in the other group means. Since these values 
+  # represent the lower bound of the data, the scale is less sensitive to the 
+  # spread of higher values.
+  
+  # For example:
+  # The value of 2.28 indicates that this particular group mean is 2.28 times 
+  # the scale factor d above the smallest mean. This means the group mean is 
+  # further from the smallest mean compared to the second smallest mean, and 
+  # helps in understanding the relative differences between the group means in 
+  # a normalized manner.
+  d <- group_means[order(group_means) == 2] - group_means[order(group_means) == 1]
+  
+  for (i in seq_len(ngroups)) {
+    if (order(group_means)[i] > 2) {
+      ratio_data[i] <- 1 + (group_means[i] - group_means[order(group_means) == 1]) / d
+    }
+  }
+  return(ratio_data)
+}
+
+
+# Adjust the variance based on alternative group sizes
+adjust_variance <- function(var_e, N, alt_group_size, ngroups) {
+  # Possibly adjust var_e based on other sample size
+  if (length(alt_group_size) == 1) {
+    var_e <- var_e * (sum(N) - ngroups)
+    N <- rep(alt_group_size, ngroups)
+    var_e <- var_e / (sum(N) - ngroups)
+  } else if (length(alt_group_size) == ngroups) {
+    var_e <- var_e * (sum(N) - ngroups)
+    N <- alt_group_size
+    var_e <- var_e / (sum(N) - ngroups)
+  } else {
+    return(paste0("The argument alt_group_size should be of length 1 or ", 
+                  ngroups, " (or NULL) but not of length ", length(alt_group_size)))
+  }
+  return(list(var_e = var_e, N = N))
+}
+
+
+# Compute the population means based on the input parameters
+compute_population_means <- function(pop_es, ratio_pop_means, var_e, ngroups) {
+  means_pop_all <- matrix(NA, ncol = ngroups, nrow = length(pop_es))
+  nr_es <- length(pop_es)
+  for (teller_es in seq_len(nr_es)) {
+    #teller_es = 1
+    
+    # Determine mean values, with ratio of ratio.m
+    # Solve for x here
+    
+    # If all equal, then set population means to all 0
+    if (length(unique(ratio_pop_means)) == 1) {
+      means_pop <- rep(0, ngroups)
+    } else {
+      fun <- function (d) {
+        means_pop = ratio_pop_means * d 
+        (1/sqrt(var_e)) * sqrt((1/ngroups) * sum((means_pop - mean(means_pop))^2)) - pop_es[teller_es] #  AANPASSSEN NAAR NIEUWE FORMULE
+      }
+      d <- uniroot(fun, lower = 0, upper = 100)$root
+      # Construct means_pop
+      means_pop <- ratio_pop_means*d
+    }
+    means_pop_all[teller_es, ] <- means_pop
+  }
+  return(means_pop_all)  
+}
+
+
+# Calculate the error probability
+calculate_error_probability <- function(object, hypos, pref_hypo, group_means, 
+                                        VCOV, control, ...) {
+  # Error probability based on complement of preferred hypothesis in data
+  nr_hypos <- dim(object$result)[1]
+  if (nr_hypos == 2 && object$comparison == "complement") { 
+    if (object$type == 'goric') {
+      error_prob <- 1 - object$result$goric.weights[pref_hypo]
+    } else {
+      error_prob <- 1 - object$result$gorica.weights[pref_hypo]
+    }
+  } else {
+    if (pref_hypo == nr_hypos && object$comparison == "unconstrained") {
+      error_prob <- "The unconstrained (i.e., the failsafe) containing all possible orderings is preferred."
+    } else {
+      H_pref <- hypos[[pref_hypo]]
+      if (is.null(object$model.org)) {
+        results_goric_pref <- goric(group_means, VCOV = VCOV,
+                                    hypotheses = list(H_pref = H_pref),
+                                    comparison = "complement",
+                                    type = object$type, 
+                                    control = control, 
+                                    ...)
+      } else {
+        fit_data <- object$model.org
+        results_goric_pref <- goric(fit_data,
+                                    hypotheses = list(H_pref = H_pref),
+                                    comparison = "complement",
+                                    type = object$type,
+                                    control = control, 
+                                    ...)
+      }
+      if (object$type == 'goric') {
+        error_prob <- results_goric_pref$result$goric.weights[2]
+      } else {
+        error_prob <- results_goric_pref$result$gorica.weights[2]
+      }
+    }
+  }
+  return(error_prob)
+}
+
+
+
+# this function is called from the goric_benchmark_anova() function
+parallel_function_means <- function(i, N, var_e, means_pop, 
+                                    hypos, pref_hypo, object, ngroups, sample, 
+                                    control, form_model_org, ...) {  
+  # Sample residuals
+  epsilon <- rnorm(sum(N), sd = sqrt(var_e))
+  
+  # original model formula 
+  if (length(form_model_org) > 0) {
+    model <- form_model_org
+    lhs <- all.vars(model)[1]
+    sample[[lhs]] <- as.matrix(sample[, 2:(1 + ngroups)]) %*% matrix(means_pop, 
+                                                                     nrow = ngroups) + epsilon
+    df_boot <- data.frame(lhs = sample[[lhs]], sample[, 2:(1 + ngroups)])
+    colnames(df_boot)[1] <- lhs
+    
+    has_intercept <- attr(terms(model), "intercept") == 1
+    rhs <- as.character(attr(terms(model), "term.labels"))
+    
+    # Create the RHS with all other variables and optionally the intercept
+    if (has_intercept) {
+      new_rhs <- "."
+    } else {
+      new_rhs <- "-1 + ."
+    }
+    
+    # Create the new formula
+    new_model <- as.formula(paste(lhs, "~", new_rhs))
+  } else {
+    new_model <- y ~ 0 + .
+    # Generate data
+    sample$y <- as.matrix(sample[, 2:(1 + ngroups)]) %*% matrix(means_pop, 
+                                                                nrow = ngroups) + epsilon
+    df_boot <- data.frame(y = sample$y, sample[, 2:(1 + ngroups)])
+  }
+  
+  
+  # Obtain fit
+  fit_boot <- lm(new_model, data = df_boot)  
+  # GORICA or GORICA depending on what is done in data
+  results_goric <- goric(fit_boot,
+                         hypotheses = hypos,
+                         comparison = object$comparison,
+                         type = object$type,
+                         control = control, 
+                         ...)
+  
+  # Return the relevant results
+  list(
+    #test  = attr(results.goric$objectList[[results.goric$objectNames]]$wt.bar, "mvtnorm"),
+    gw  = results_goric$result[pref_hypo, 7], # goric(a) weight
+    rgw = results_goric$ratio.gw[pref_hypo, ], # ratio goric(a) weights
+    rlw = results_goric$ratio.lw[pref_hypo, ], # ratio likelihood weights
+    ld  = (results_goric$result$loglik[pref_hypo] - results_goric$result$loglik)
+  )
+}
+
+
+# this function is called from the benchmark_asymp() function
+parallel_function_asymp <- function(i, est, VCOV, hypos, pref_hypo, comparison,
+                                    type, control, ...) {  
+  
+  results_goric <- goric(est[i, ], VCOV = VCOV,
+                         hypotheses = hypos,
+                         comparison = comparison,
+                         type = type,
+                         control = control, 
+                         ...)
+  
+  # Return the relevant results
+  list(
+    #test  = attr(results.goric$objectList[[results.goric$objectNames]]$wt.bar, "mvtnorm"),
+    gw  = results_goric$result[pref_hypo, 7], # goric(a) weight
+    rgw = results_goric$ratio.gw[pref_hypo, ], # ratio goric(a) weights
+    rlw = results_goric$ratio.lw[pref_hypo, ], # ratio likelihood weights
+    ld  = (results_goric$result$loglik[pref_hypo] - results_goric$result$loglik)
+  )
+}
+
+# called by the benchmark.print() function
+print_section <- function(header, content_printer, nchar, text_color, reset) {
+  cat("\n")
+  cat(strrep("=", nchar), "\n")
+  cat(paste0(text_color, header, reset), "\n")
+  cat(strrep("-", nchar), "\n")
+  content_printer()
+  #cat(strrep("-", nchar), "\n")
+  cat("\n")
+}
+
+# called by the benchmark.print() function
+print_rounded_es_value <- function(df, pop_es, model_type, text_color, reset) {
+  if (model_type == "benchmark_asymp") {
+    pop_es_value <- gsub("pop_est = ", "", pop_es)
+  } else {
+    pop_es_value <- gsub("pop_es = ", "", pop_es)  
+  }
+  cat(sprintf("Population effect size = %s%s%s\n", text_color, pop_es_value, reset))
+  
+  format_value <- function(value) {
+    if (abs(value) >= 1000 || abs(value) <= 0.001 && value != 0) {
+      return(sprintf("%.3e", value))  
+    } else {
+      return(sprintf("%.3f", value))  
+    }
+  }
+  #formatted_column <- sprintf("%.3f", df)
+  formatted_values <- sapply(as.numeric(df), format_value)
+  formatted_df <- `dim<-`(formatted_values, dim(df))
+  rownames(formatted_df) <- rownames(df)
+  colnames(formatted_df) <- colnames(df)
+  print(formatted_df, row.names = TRUE, quote = FALSE)
+  cat("\n")
+}
+
+# called by the benchmark.print() function
+print_formatted_matrix <- function(mat, text_color, reset) {
+  cat("Population Estimates:\n")
+  cat(sprintf("%-10s", ""))
+  for (colname in colnames(mat)) {
+    cat(sprintf(" %7s", colname))
+  }
+  cat("\n")
+  for (i in 1:nrow(mat)) {
+    cat(sprintf("%-10s", rownames(mat)[i]))
+    for (j in 1:ncol(mat)) {
+      cat(sprintf(" %s%7s%s", text_color, mat[i, j], reset))
+    }
+    cat("\n")
+  }
+}
+
+# called by the benchmark_asymp() function
+check_rhs_constants <- function(rhs_list) {
+  constants_check <- lapply(rhs_list, function(element) {
+    return(any(element != 0))
+  })
+  hypotheses_with_constants <- names(constants_check)[unlist(constants_check)]
+  if (length(hypotheses_with_constants) > 0) {
+    warning_message <- paste0("Restriktor Warning: The following hypotheses contain constants",
+                              "greater or less than 0: ", 
+                              paste(hypotheses_with_constants, collapse = ", "),
+                              ". The default population estimates are likely incorrect.",
+                              "Consider providing custom population estimates via the",
+                              "pop_est argument.")
+    warning(warning_message, call. = FALSE)
+  }
+}

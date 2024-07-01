a3ed9d4888cffa1dda012202585fc10975fb2e00
LeonardV <lgf.vanbrabant@gmail.com>
2024-07-01 14:17:19 +0200

    fix issue if multiple hyposeses are specified

--- R/goric_benchmark_plot.R
+++ R/goric_benchmark_plot.R
@@ -1,4 +1,5 @@
-plot.benchmark <- function(x, output_type = "gw", CI = 0.95, x_lim = c(), ...) {
+plot.benchmark <- function(x, output_type = c("rgw", "rlw", "gw", "ld"), percentile = c(0.025, 0.975), 
+                           x_lim = c(), ...) {
   
   # Ensure the object is of class 'benchmark_means'
   if (!inherits(x, "benchmark")) {
@@ -6,15 +7,14 @@ plot.benchmark <- function(x, output_type = "gw", CI = 0.95, x_lim = c(), ...) {
   }
   
   # Check if the output_type is valid
-  valid_types <- c("gw", "rgw", "rlw", "ld")
-  if (!output_type %in% valid_types) {
-    stop("Restriktor ERROR: Invalid output_type. Valid types are 'gw', 'rgw', 'rlw', 'ld'.", call. = FALSE)
-  }
+  output_type <- match.arg(output_type, c("rgw", "rlw", "gw", "ld"))
+  # if (!output_type %in% valid_types) {
+  #   stop("Restriktor ERROR: Invalid output_type. Valid types are 'gw', 'rgw', 'rlw', 'ld'.", call. = FALSE)
+  # }
   
-  quant <- CI
   legend_lab <- names(x$benchmarks_goric_weights)
   # first letter to upper-case
-  goric_type <- paste0(toupper(substring(x$type, 1, 1)), substring(x$type, 2))
+  goric_type <- toupper(x$type) #paste0(toupper(substring(x$type, 1, 1)), substring(x$type, 2))
   
   comparison <- x$comparison
   pref_hypo_name <- x$pref_hypo_name
@@ -23,33 +23,41 @@ plot.benchmark <- function(x, output_type = "gw", CI = 0.95, x_lim = c(), ...) {
   if (output_type == "gw") {
     df <- as.data.frame(x$combined_values$gw_combined)  
     sample_value <- x$benchmarks_goric_weights[[1]][1]
-    xlabel <- paste0(goric_type, "-weights")
+    xlabel <- paste(goric_type, "weights")
     title <- paste0("Benchmark: ", goric_type, "-weight distribution for preferred hypothesis ", pref_hypo_name)
   } else if (output_type == "rgw") {
     df <- as.data.frame(x$combined_values$rgw_combined)  
-    df <- df[, !colMeans(df) == 1, drop = FALSE]
+    #df <- df[, !colMeans(df) == 1, drop = FALSE]
     sample_value <- x$benchmarks_ratio_goric_weights[[1]][1, 1]
-    xlabel <- paste0("Ratio ", goric_type, "-weights")
+    xlabel <- paste0("Ratio ", goric_type, " weights")
     title <- paste0("Benchmark: Ratio ", goric_type, "-weight distribution for preferred hypothesis ", pref_hypo_name)
   } else if (output_type == "rlw") {
     df <- as.data.frame(x$combined_values$rlw_combined)  
-    df <- df[, !colMeans(df) == 1, drop = FALSE]
+    #df <- df[, !colMeans(df) == 1, drop = FALSE]
     sample_value <- x$benchmarks_ratio_ll_weights[[1]][1, 1]
-    xlabel <- "Ratio likelihood-weights"
+    xlabel <- "Ratio likelihood weights"
     title <- paste0("Benchmark: Ratio likelihood weight distribution for preferred hypothesis ", pref_hypo_name)
   } else if (output_type == "ld") {
     df <- as.data.frame(x$combined_values$ld_combined)
-    df <- df[, !colMeans(df) == 0, drop = FALSE]
+    #df <- df[, !colMeans(df) == 0, drop = FALSE]
     sample_value <- x$benchmarks_difLL[[1]][1, 1]
     xlabel <- "Likelihood difference"
     title <- paste0("Benchmark: Likelihood difference distribution for preferred hypothesis ", pref_hypo_name)
   }
   
-  names(df) <- legend_lab
+  if (output_type != "gw") {
+    ratio_names <- rownames(x$benchmarks_ratio_goric_weights[[1]])
+    result_vector <- outer(legend_lab, ratio_names, function(x, y) paste(x, " (", y, ")", sep = ""))
+    result_vector <- as.vector(result_vector)
+    names(df) <- result_vector
+  } else {
+    result_vector <- legend_lab
+    names(df) <- result_vector
+  }
   
-  # Ensure quant is valid
-  if (quant <= 0 || quant >= 1) {
-    stop("Restriktor ERROR: a confidence interval should be between 0 and 1.", call. = FALSE)
+  # Ensure percentile is valid
+  if (any(percentile <= 0 | percentile >= 1)) {
+    stop("Restriktor ERROR: a percentile should be between 0 and 1.", call. = FALSE)
   }
   
   # Reshape the dataframe 
@@ -57,40 +65,46 @@ plot.benchmark <- function(x, output_type = "gw", CI = 0.95, x_lim = c(), ...) {
                      varying = list(1:length(df)),#list(names(df)[grepl("^pop", names(df))]), 
                      v.names = "Value", 
                      timevar = "Group", 
-                     times = legend_lab, #names(df)[grepl("^pop", names(df))], 
+                     times = result_vector, #names(df)[grepl("^pop", names(df))], 
                      direction = "long")
   
   row.names(df_long) <- NULL
   
   # Rename the Group column to replace triple dots with equals sign
-  df_long$Group <- gsub("\\.\\.\\.", " = ", df_long$Group)
-  df_long$Group <- factor(df_long$Group, levels = legend_lab)
+  #df_long$Group <- gsub("\\.\\.\\.", " = ", df_long$Group)
+  df_long$Group <- factor(df_long$Group, levels = result_vector)
   
   # Calculate the custom confidence intervals
-  ci_df <- aggregate(Value ~ Group, data = df_long, function(x) {
-    c(lower = quantile(x, probs = (1 - quant) / 2), 
-      upper = quantile(x, probs = 1 - (1 - quant) / 2), mean = mean(x))
+  # ci_df <- aggregate(Value ~ Group, data = df_long, function(x) {
+  #   c(lower = quantile(x, probs = (1 - CI) / 2), 
+  #     upper = quantile(x, probs = 1 - (1 - CI) / 2), mean = mean(x))
+  # })
+  # Calculate the custom percentile intervals
+  percentile_df <- aggregate(Value ~ Group, data = df_long, function(x) {
+    c(lower = quantile(x, probs = percentile[1]), 
+      upper = quantile(x, probs = percentile[2]), mean = mean(x))
   })
-  ci_df <- data.frame(Group = ci_df$Group, ci_df$Value)
-  names(ci_df) <- c("Group", "lower", "upper", "mean")
+  
+  percentile_df <- data.frame(Group = percentile_df$Group, percentile_df$Value)
+  names(percentile_df) <- c("Group", "lower", "upper", "mean")
   
   # Calculate maximum density values for each group 
   max_density <- max(sapply(split(df_long$Value, df_long$Group), function(x) max(density(x)$y)))
   
   # Prepare text for the annotations with fixed digit formatting
-  ci_df$label <- paste(ci_df$Group, ": ", sprintf("%.0f", quant * 100), "% CI = [", 
-                       sprintf("%.3f", ci_df$lower), ", ", 
-                       sprintf("%.3f", ci_df$upper), "]", sep = "")
+  # percentile_df$label <- paste(percentile_df$Group, ": ", sprintf("%.0f", quant * 100), "th percentile = [", 
+  #                      sprintf("%.3f", percentile_df$lower), ", ", 
+  #                      sprintf("%.3f", percentile_df$upper), "]", sep = "")
 
   first_group <- levels(factor(df_long$Group))[1]
   first_group_data <- subset(df_long, Group == first_group)
-  ci_first_group <- ci_df[ci_df$Group == first_group, ]
+  percentile_first_group <- percentile_df[percentile_df$Group == first_group, ]
   
   first_group_color <- scales::brewer_pal(palette = "Set3")(length(unique(df_long$Group)))[1]
   
   sample_value <- round(sample_value, 3)
-  ci_first_group_lower <- round(ci_first_group$lower, 3)
-  ci_first_group_upper <- round(ci_first_group$upper, 3)
+  percentile_first_group_lower <- round(percentile_first_group$lower, 3)
+  percentile_first_group_upper <- round(percentile_first_group$upper, 3)
   
   # Plotting with ggplot
   p <- ggplot(df_long, aes(x = Value, fill = Group)) +
@@ -98,11 +112,13 @@ plot.benchmark <- function(x, output_type = "gw", CI = 0.95, x_lim = c(), ...) {
     geom_segment(aes(x = sample_value, xend = sample_value, y = 0, yend = max_density),
                  linetype = "solid", color = "red", linewidth = 1,
                  show.legend = FALSE) +
-    geom_segment(aes(x = ci_first_group_lower, xend = ci_first_group_lower, y = 0, yend = max_density),
+    geom_segment(aes(x = percentile_first_group_lower, 
+                     xend = percentile_first_group_lower, y = 0, yend = max_density),
                  linetype = "solid", color = first_group_color, linewidth = 1) + 
-    geom_segment(aes(x = ci_first_group_upper, xend = ci_first_group_upper, y = 0, yend = max_density),
+    geom_segment(aes(x = percentile_first_group_upper, 
+                     xend = percentile_first_group_upper, y = 0, yend = max_density),
                  linetype = "solid", color = first_group_color, linewidth = 1) + 
-    geom_segment(aes(x = 0, xend = 0, y = 0, yend = 0, color = paste0(CI * 100, "% CI")),
+    geom_segment(aes(x = 0, xend = 0, y = 0, yend = 0, color = paste0("[", paste(percentile*100, collapse = ", "), "]", "th percentiles")),
                  linetype = "solid", linewidth = 0) +
     geom_segment(aes(x = 0, xend = 0, y = 0, yend = 0, color = paste0("Observed value ", sample_value)),
                  linetype = "solid", linewidth = 0) +
@@ -119,7 +135,7 @@ plot.benchmark <- function(x, output_type = "gw", CI = 0.95, x_lim = c(), ...) {
           plot.title = element_text(size = 12)) +
     scale_fill_brewer(palette = "Set3") +
     scale_color_manual(name = c("", ""), 
-                       values = c(setNames(first_group_color, paste0(CI * 100, "% CI")),
+                       values = c(setNames(first_group_color, paste0("[", paste(percentile*100, collapse = ", "), "]", "th percentiles")),
                                   setNames("red", paste0("Observed value ", sample_value)))) +
     theme(legend.key = element_rect(fill = "white"))  
   
@@ -134,7 +150,7 @@ plot.benchmark <- function(x, output_type = "gw", CI = 0.95, x_lim = c(), ...) {
   #y_max <- plot_lims[2]
   
   # Calculate the spacing for the annotations
-  #num_annotations <- nrow(ci_df) + 1
+  #num_annotations <- nrow(percentile_df) + 1
   #spacing <- y_max / 20#(num_annotations + 1)
   
   # Determine the position for the annotation to ensure it stays within the plot area

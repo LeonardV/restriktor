9c2189895820f71307025472106275deaf5a7cc2
LeonardV <lgf.vanbrabant@gmail.com>
2024-07-01 22:37:33 +0200

    add hypothesis_comparison argument and many more improvements

--- R/goric_benchmark_plot.R
+++ R/goric_benchmark_plot.R
@@ -1,5 +1,6 @@
-plot.benchmark <- function(x, output_type = c("rgw", "rlw", "gw", "ld"), percentile = c(0.025, 0.975), 
-                           x_lim = c(), ...) {
+plot.benchmark <- function(x, output_type = c("rgw", "rlw", "gw", "ld"), 
+                           hypothesis_comparison = NULL,
+                           percentiles = c(0.025, 0.975), x_lim = c(), ...) {
   
   # Ensure the object is of class 'benchmark_means'
   if (!inherits(x, "benchmark")) {
@@ -27,20 +28,17 @@ plot.benchmark <- function(x, output_type = c("rgw", "rlw", "gw", "ld"), percent
     title <- paste0("Benchmark: ", goric_type, "-weight distribution for preferred hypothesis ", pref_hypo_name)
   } else if (output_type == "rgw") {
     df <- as.data.frame(x$combined_values$rgw_combined)  
-    #df <- df[, !colMeans(df) == 1, drop = FALSE]
-    sample_value <- x$benchmarks_ratio_goric_weights[[1]][1, 1]
+    sample_value <- x$benchmarks_ratio_goric_weights[[1]][, 1]
     xlabel <- paste0("Ratio ", goric_type, " weights")
     title <- paste0("Benchmark: Ratio ", goric_type, "-weight distribution for preferred hypothesis ", pref_hypo_name)
   } else if (output_type == "rlw") {
     df <- as.data.frame(x$combined_values$rlw_combined)  
-    #df <- df[, !colMeans(df) == 1, drop = FALSE]
-    sample_value <- x$benchmarks_ratio_ll_weights[[1]][1, 1]
+    sample_value <- x$benchmarks_ratio_ll_weights[[1]][, 1]
     xlabel <- "Ratio likelihood weights"
     title <- paste0("Benchmark: Ratio likelihood weight distribution for preferred hypothesis ", pref_hypo_name)
   } else if (output_type == "ld") {
     df <- as.data.frame(x$combined_values$ld_combined)
-    #df <- df[, !colMeans(df) == 0, drop = FALSE]
-    sample_value <- x$benchmarks_difLL[[1]][1, 1]
+    sample_value <- x$benchmarks_difLL[[1]][, 1]
     xlabel <- "Likelihood difference"
     title <- paste0("Benchmark: Likelihood difference distribution for preferred hypothesis ", pref_hypo_name)
   }
@@ -50,13 +48,18 @@ plot.benchmark <- function(x, output_type = c("rgw", "rlw", "gw", "ld"), percent
     result_vector <- outer(legend_lab, ratio_names, function(x, y) paste(x, " (", y, ")", sep = ""))
     result_vector <- as.vector(result_vector)
     names(df) <- result_vector
+    if (!is.null(hypothesis_comparison) && output_type != "gw") {
+      sample_value <- filter_vector(sample_value, hypothesis_comparison)
+      df <- filter_columns(df, hypothesis_comparison)
+      result_vector <- names(df)
+    }
   } else {
     result_vector <- legend_lab
     names(df) <- result_vector
   }
-  
-  # Ensure percentile is valid
-  if (any(percentile <= 0 | percentile >= 1)) {
+
+  # Ensure percentiles is valid
+  if (any(percentiles <= 0 | percentiles >= 1)) {
     stop("Restriktor ERROR: a percentile should be between 0 and 1.", call. = FALSE)
   }
   
@@ -81,8 +84,8 @@ plot.benchmark <- function(x, output_type = c("rgw", "rlw", "gw", "ld"), percent
   # })
   # Calculate the custom percentile intervals
   percentile_df <- aggregate(Value ~ Group, data = df_long, function(x) {
-    c(lower = quantile(x, probs = percentile[1]), 
-      upper = quantile(x, probs = percentile[2]), mean = mean(x))
+    c(lower = quantile(x, probs = percentiles[1]), 
+      upper = quantile(x, probs = percentiles[2]), mean = mean(x))
   })
   
   percentile_df <- data.frame(Group = percentile_df$Group, percentile_df$Value)
@@ -100,49 +103,71 @@ plot.benchmark <- function(x, output_type = c("rgw", "rlw", "gw", "ld"), percent
   first_group_data <- subset(df_long, Group == first_group)
   percentile_first_group <- percentile_df[percentile_df$Group == first_group, ]
   
-  first_group_color <- scales::brewer_pal(palette = "Set3")(length(unique(df_long$Group)))[1]
+  group_color <- scales::brewer_pal(palette = "Set3")(length(unique(df_long$Group)))
+  first_group_color <- group_color[1]
   
-  sample_value <- round(sample_value, 3)
   percentile_first_group_lower <- round(percentile_first_group$lower, 3)
   percentile_first_group_upper <- round(percentile_first_group$upper, 3)
   
-  # Plotting with ggplot
+
+  #############################################
+  sample_segments <- lapply(seq_along(sample_value), function(i) {
+    label <- paste0(format_value(sample_value[i]), " (", names(sample_value)[i], ")")
+    geom_segment(aes(x = sample_value[i], xend = sample_value[i], y = 0, yend = max_density,
+                     color = label, linetype = label), linewidth = 1, show.legend = TRUE)
+  })
+  
+  # Generate percentile labels dynamically
+  percentile_label <- paste0("[", percentiles[1]*100, ", ", percentiles[2]*100, "]th Percentiles")
+  observed_label <- paste0(sapply(sample_value, format_value), " (", names(sample_value), ")")
+  
+  linetypes <- c("solid", "dashed", "dotted", "dotdash", "longdash", "twodash")
+  observed_linetypes <- linetypes[1:length(sample_value)]
+  
+  # Create the plot
   p <- ggplot(df_long, aes(x = Value, fill = Group)) +
     geom_density(alpha = 0.5) +  # Density plots for multiple groups
-    geom_segment(aes(x = sample_value, xend = sample_value, y = 0, yend = max_density),
-                 linetype = "solid", color = "red", linewidth = 1,
-                 show.legend = FALSE) +
     geom_segment(aes(x = percentile_first_group_lower, 
-                     xend = percentile_first_group_lower, y = 0, yend = max_density),
-                 linetype = "solid", color = first_group_color, linewidth = 1) + 
+                     xend = percentile_first_group_lower, y = 0, yend = max_density,
+                     color = percentile_label, linetype = percentile_label), linewidth = 1) + 
     geom_segment(aes(x = percentile_first_group_upper, 
-                     xend = percentile_first_group_upper, y = 0, yend = max_density),
-                 linetype = "solid", color = first_group_color, linewidth = 1) + 
-    geom_segment(aes(x = 0, xend = 0, y = 0, yend = 0, color = paste0("[", paste(percentile*100, collapse = ", "), "]", "th percentiles")),
-                 linetype = "solid", linewidth = 0) +
-    geom_segment(aes(x = 0, xend = 0, y = 0, yend = 0, color = paste0("Observed value ", sample_value)),
-                 linetype = "solid", linewidth = 0) +
+                     xend = percentile_first_group_upper, y = 0, yend = max_density,
+                     color = percentile_label, linetype = percentile_label), linewidth = 1) + 
     ggtitle(title) + 
     xlab(xlabel) + ylab("Density") + 
-    {if (inherits(x, "benchmark_asymp")) {
-      labs(fill = "Effect-Size")
-    } else {
-      labs(fill = "Effect-Size f")
-    }}  +
     theme(axis.text = element_text(size = 11),
           axis.title.x = element_text(size = 12, margin = margin(t = 10)),
           axis.title.y = element_text(size = 12, margin = margin(r = 10)),
           plot.title = element_text(size = 12)) +
     scale_fill_brewer(palette = "Set3") +
-    scale_color_manual(name = c("", ""), 
-                       values = c(setNames(first_group_color, paste0("[", paste(percentile*100, collapse = ", "), "]", "th percentiles")),
-                                  setNames("red", paste0("Observed value ", sample_value)))) +
-    theme(legend.key = element_rect(fill = "white"))  
+    scale_color_manual(name = "", 
+                       values = c(setNames(rep("red", length(sample_value)), observed_label),
+                                  setNames(first_group_color, percentile_label)),
+                       breaks = c(observed_label, percentile_label),
+                       guide = "none") +
+    scale_linetype_manual(name = "Observed Values", 
+                          values = c(setNames(observed_linetypes, observed_label),
+                                     setNames("solid", percentile_label))) + 
+    theme(legend.key = element_rect(fill = "white")) +
+    guides(fill = guide_legend(override.aes = list(color = NA)),  # Maintain filled boxes in legend
+           linetype = guide_legend(override.aes = list(color = c(first_group_color, rep("red", length(sample_value))))))  # Set legend colors
   
-   if (!is.null(x_lim) && length(x_lim) == 2) {
-     p <- p + coord_cartesian(xlim = x_lim)
-   }
+  # Add the segments for sample values
+  for (segment in sample_segments) {
+    p <- p + segment
+  }
   
+  if (inherits(x, "benchmark_asymp")) {
+     p <- p + labs(fill = "Effect-Size Estimates")
+  } else {
+     p <- p + labs(fill = "Effect-Size Cohens f")
+  } 
+
+  
+  if (!is.null(x_lim) && length(x_lim) == 2) {
+    p <- p + coord_cartesian(xlim = x_lim)
+  } 
+
   # Render the plot to get the y-axis limits
   #plot_lims <- ggplot_build(p)$layout$panel_params[[1]]$y.range
   

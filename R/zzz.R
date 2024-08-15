701bedaeacb3f2df6186fffcd58d7edbcbdf0fbd
LeonardV <lgf.vanbrabant@gmail.com>
2024-08-15 12:16:26 +0200

    add globalvariables

--- R/zzz.R
+++ R/zzz.R
@@ -2,10 +2,10 @@
     version <- read.dcf(file=system.file("DESCRIPTION", package=pkgname),
                       fields="Version")
     packageStartupMessage("This is ",paste(pkgname, version))
-    #packageStartupMessage(pkgname, " is BETA software! Please report any bugs.")
     packageStartupMessage("Please report any bugs to info@restriktor.org")
 }
 
 utils::globalVariables(c("Group", "Value", "green", "reset", 
                          "Group_hypo_comparison", "Group_pop_values", 
-                         "sample_value", "x", "y"))
+                         "sample_value", "x", "y", "percentile_value",
+                         "percentile_label"))

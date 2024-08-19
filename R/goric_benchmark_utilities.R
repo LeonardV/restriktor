5df89506186070449d8effbe907646f0dd76fb05
LeonardV <lgf.vanbrabant@gmail.com>
2024-08-19 23:00:25 +0200

    add try() to goric function

--- R/goric_benchmark_utilities.R
+++ R/goric_benchmark_utilities.R
@@ -39,6 +39,16 @@ calculate_power <- function(density_h1, critical_value) {
   sum(density_h1$y[density_h1$x > critical_value]) * mean(diff(density_h1$x))
 }
 
+# Function to calculate density
+# nrd0
+calculate_density <- function(data, var, sample_value) {
+  #scaled_var <- scale(data[[var]], center = TRUE, scale = TRUE)
+  #dens <- density(scaled_var, kernel = "gaussian", na.rm = TRUE, bw = "nrd0")
+  dens <- density(data[[var]], kernel = "gaussian", na.rm = TRUE, bw = "nrd0")
+  data.frame(x = dens$x, y = dens$y, sample_value = sample_value)
+}
+
+
 # Extract the parts within the parentheses
 extract_in_parentheses <- function(name) {
   regmatches(name, regexpr("\\(.*\\)", name))
@@ -52,6 +62,28 @@ remove_spaces_in_parentheses <- function(string) {
 
 construct_colnames <- function(list_name, colnames, pref_hypo_name) {
   remove_spaces_in_parentheses(paste0(list_name, " (", pref_hypo_name, " ", colnames, ")", sep = ""))
+}
+
+combine_matrices_cbind <- function(lst) {
+  df_list <- lapply(lst, as.data.frame)
+  combined_df <- do.call(cbind, df_list)
+  return(combined_df)
+}
+
+
+combine_matrices_cbind <- function(lst) {
+  # Vind het maximum aantal rijen in de lijst
+  max_rows <- max(sapply(lst, nrow))
+  
+  # Zet de matrices om naar data frames en vul met NA waar nodig
+  df_list <- lapply(lst, function(x) {
+    as.data.frame(rbind(x, matrix(NA, nrow = max_rows - nrow(x), ncol = ncol(x))))
+  })
+  
+  # Combineer de data frames kolomgewijs
+  combined_df <- do.call(cbind, df_list)
+  
+  return(combined_df)
 }
 
 
@@ -244,12 +276,17 @@ parallel_function_means <- function(i, N, var_e, means_pop,
   # Obtain fit
   fit_boot <- lm(new_model, data = df_boot)  
   # GORICA or GORICA depending on what is done in data
-  results_goric <- goric(fit_boot,
-                         hypotheses = hypos,
-                         comparison = object$comparison,
-                         type = object$type,
-                         control = control, 
-                         ...)
+  results_goric <- try(goric(fit_boot,
+                             hypotheses = hypos,
+                             comparison = object$comparison,
+                             type = object$type,
+                             control = control, 
+                             ...), silent = TRUE
+  )
+  
+  if (inherits(results_goric, "try-error")) {
+    return(as.numeric(NA))
+  }
   
   # Return the relevant results
   ld_names <- names(results_goric$ratio.gw[pref_hypo, ])
@@ -270,12 +307,18 @@ parallel_function_means <- function(i, N, var_e, means_pop,
 parallel_function_asymp <- function(i, est, VCOV, hypos, pref_hypo, comparison,
                                     type, control, ...) {  
   
-  results_goric <- goric(est[i, ], VCOV = VCOV,
-                         hypotheses = hypos,
-                         comparison = comparison,
-                         type = type,
-                         control = control, 
-                         ...)
+  results_goric <- try(goric(est[i, ], VCOV = VCOV,
+                             hypotheses = hypos,
+                             comparison = comparison,
+                             type = type,
+                             control = control, 
+                             ...), silent = TRUE
+  )
+  
+  if (inherits(results_goric, "try-error")) {
+    return(as.numeric(NA))
+  }
+  
   
   # Return the relevant results
   ld_names <- names(results_goric$ratio.gw[pref_hypo, ])
@@ -290,6 +333,153 @@ parallel_function_asymp <- function(i, est, VCOV, hypos, pref_hypo, comparison,
     ld  = ld
   )
 }
+
+
+# Define a function to extract and combine values from all elements in each pop_es list
+extract_and_combine_values <- function(pop_es_list, value_name) {
+  # remove empty lists
+  pop_es_list <- pop_es_list[!sapply(pop_es_list, function(x) any(is.na(x)))]
+  do.call(rbind, lapply(pop_es_list, function(sub_list) sub_list[[value_name]]))
+}
+
+
+## 
+get_results_benchmark <- function(x, object, pref_hypo, pref_hypo_name, 
+                                  quant, names_quant, nr.hypos) {
+  
+  results <- x
+
+  # Use lapply to apply the extract_and_combine_values function to each element in the results list
+  gw_combined  <- lapply(results, function(pop_es_list) extract_and_combine_values(pop_es_list, "gw"))
+  rgw_combined <- lapply(results, function(pop_es_list) extract_and_combine_values(pop_es_list, "rgw"))
+  rlw_combined <- lapply(results, function(pop_es_list) extract_and_combine_values(pop_es_list, "rlw"))
+  ld_combined  <- lapply(results, function(pop_es_list) extract_and_combine_values(pop_es_list, "ld"))
+  
+  # Calculate CI_benchmarks_gw for each pop_es category
+  CI_benchmarks_gw <- lapply(gw_combined, function(gw_values) {
+    CI_benchmarks_gw <- matrix(c(object$result[pref_hypo, 7], quantile(gw_values, quant, na.rm = TRUE)), nrow = 1)
+    colnames(CI_benchmarks_gw) <- names_quant
+    rownames(CI_benchmarks_gw) <- pref_hypo_name
+    CI_benchmarks_gw
+  })
+  
+  
+  # Initialize matrices to store CI benchmarks for current pop_es category
+  CI_benchmarks_rgw <- matrix(NA, nrow = nr.hypos, ncol = 1 + length(quant))
+  CI_benchmarks_rlw <- matrix(NA, nrow = nr.hypos, ncol = 1 + length(quant))
+  CI_benchmarks_rlw_ge1 <- matrix(NA, nrow = nr.hypos, ncol = 1 + length(quant))
+  CI_benchmarks_ld <- matrix(NA, nrow = nr.hypos, ncol = 1 + length(quant))
+  CI_benchmarks_ld_ge0 <- matrix(NA, nrow = nr.hypos, ncol = 1 + length(quant))
+  
+  # Fill the first column with sample values
+  CI_benchmarks_rgw[, 1] <- object$ratio.gw[pref_hypo,] 
+  CI_benchmarks_rlw[, 1] <- object$ratio.lw[pref_hypo,] 
+  for (j in seq_len(nr.hypos)) {
+    if (object$ratio.lw[pref_hypo, j] >= 1) {
+      CI_benchmarks_rlw_ge1[j, 1] <- object$ratio.lw[pref_hypo, j] 
+    } else {
+      CI_benchmarks_rlw_ge1[j, 1] <- 1 / object$ratio.lw[pref_hypo, j] 
+    }
+  }
+  CI_benchmarks_ld[, 1] <- object$result$loglik[pref_hypo] - object$result$loglik 
+  CI_benchmarks_ld_ge0[, 1] <- abs(object$result$loglik[pref_hypo] - object$result$loglik) 
+  
+  CI_benchmarks_rgw_all <- list()
+  CI_benchmarks_rlw_all <- list()
+  CI_benchmarks_rlw_ge1_all <- list()
+  CI_benchmarks_ld_all <- list()
+  CI_benchmarks_ld_ge0_all <- list()
+  
+  # Loop through each pop_es category to fill in the CI benchmark lists
+  for (name in names(results)) {
+    rgw_combined_values <- rgw_combined[[name]]
+    rlw_combined_values  <- rlw_combined[[name]]
+    ld_combined_values  <- ld_combined[[name]]
+    
+    # Prepare rlw_ge1 and ld_ge0 matrices
+    rlw_ge1 <- rlw_combined_values
+    rlw_ge1[rlw_combined_values < 1] <- 1 / rlw_combined_values[rlw_combined_values < 1]
+    ld_ge0 <- abs(ld_combined_values)
+    
+    # Loop through the hypotheses and calculate the quantiles
+    for (j in seq_len(nr.hypos)) {
+      CI_benchmarks_rgw[j, 2:(1 + length(quant))] <- quantile(rgw_combined_values[, j], quant, na.rm = TRUE)
+      CI_benchmarks_rlw[j, 2:(1 + length(quant))] <- quantile(rlw_combined_values[, j], quant, na.rm = TRUE)
+      CI_benchmarks_rlw_ge1[j, 2:(1 + length(quant))] <- quantile(rlw_ge1[, j], quant, na.rm = TRUE)
+      CI_benchmarks_ld[j, 2:(1 + length(quant))] <- quantile(ld_combined_values[, j], quant, na.rm = TRUE)
+      CI_benchmarks_ld_ge0[j, 2:(1 + length(quant))] <- quantile(ld_ge0[, j], quant, na.rm = TRUE)
+    }
+    
+    # Set column names for the CI benchmarks
+    colnames(CI_benchmarks_rgw) <- colnames(CI_benchmarks_rlw) <- 
+      colnames(CI_benchmarks_rlw_ge1) <- colnames(CI_benchmarks_ld) <- 
+      colnames(CI_benchmarks_ld_ge0) <- names_quant
+    
+    # Set row names for the CI benchmarks
+    rownames(CI_benchmarks_rgw) <- rownames(CI_benchmarks_rlw) <- 
+      rownames(CI_benchmarks_rlw_ge1) <- rownames(CI_benchmarks_ld) <- 
+      rownames(CI_benchmarks_ld_ge0) <- paste(pref_hypo_name, names(object$ratio.gw[pref_hypo, ]))
+    
+    # Store CI benchmarks in lists
+    CI_benchmarks_rgw_all[[name]] <- CI_benchmarks_rgw
+    CI_benchmarks_rlw_all[[name]] <- CI_benchmarks_rlw
+    CI_benchmarks_rlw_ge1_all[[name]] <- CI_benchmarks_rlw_ge1
+    CI_benchmarks_ld_all[[name]] <- CI_benchmarks_ld
+    CI_benchmarks_ld_ge0_all[[name]] <- CI_benchmarks_ld_ge0
+  } 
+  
+  
+  CI_benchmarks_rgw_all_cleaned <- lapply(CI_benchmarks_rgw_all, function(pop_es_list) {
+    remove_single_value_rows(pop_es_list, 1)
+  })
+  
+  CI_benchmarks_rlw_all_cleaned <- lapply(CI_benchmarks_rlw_all, function(pop_es_list) {
+    remove_single_value_rows(pop_es_list, 1)
+  })
+  
+  CI_benchmarks_rlw_ge1_all_cleaned <- lapply(CI_benchmarks_rlw_ge1_all, function(pop_es_list) {
+    remove_single_value_rows(pop_es_list, 1)
+  })
+  
+  CI_benchmarks_ld_all_cleaned <- lapply(CI_benchmarks_ld_all, function(pop_es_list) {
+    remove_single_value_rows(pop_es_list, 0)
+  })
+  
+  CI_benchmarks_ld_ge0_all_cleaned <- lapply(CI_benchmarks_ld_ge0_all, function(pop_es_list) {
+    remove_single_value_rows(pop_es_list, 0)
+  })
+  
+  
+  rgw_combined <- lapply(rgw_combined, function(pop_es_list) {
+    remove_single_value_col(pop_es_list, 1)
+  })
+  
+  rlw_combined <- lapply(rlw_combined, function(pop_es_list) {
+    remove_single_value_col(pop_es_list, 1)
+  })
+  
+  ld_combined <- lapply(ld_combined, function(pop_es_list) {
+    remove_single_value_col(pop_es_list, 0)
+  })
+  
+  
+  OUT <- list(
+    benchmarks_gw = CI_benchmarks_gw,
+    benchmarks_rgw = CI_benchmarks_rgw_all_cleaned,
+    benchmarks_rlw = CI_benchmarks_rlw_all_cleaned,
+    benchmarks_rlw_ge1 = CI_benchmarks_rlw_ge1_all_cleaned,
+    benchmarks_difLL = CI_benchmarks_ld_all_cleaned,
+    benchmarks_absdifLL = CI_benchmarks_ld_ge0_all_cleaned,
+    combined_values = list(gw_combined = gw_combined, 
+                           rgw_combined = rgw_combined, 
+                           rlw_combined = rlw_combined, 
+                           ld_combined = ld_combined)
+  )
+  
+  return(OUT)
+}
+
+
 
 # called by the benchmark.print() function
 print_section <- function(header, content_printer, nchar, text_color, reset) {

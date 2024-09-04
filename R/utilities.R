2d998956d7870402cb7fa475ec61dc2852d620a5
LeonardV <lgf.vanbrabant@gmail.com>
2024-09-04 16:52:44 +0200

    move functions to goric_utilities.R

--- R/utilities.R
+++ R/utilities.R
@@ -18,15 +18,6 @@ coef.restriktor <- function(object, ...)  {
 }
 
 
-coef.con_goric <- function(object, ...)  {
-  return(object$ormle$b.restr)
-}
-
-coef.gorica_est <- function(object, ...)  {
-  return(object$b.restr)
-}
-
-
 logLik.restriktor <- function(object, ...) {
   return(object$loglik)
 }
@@ -150,44 +141,6 @@ format_numeric <- function(x, digits = 3) {
 }
 
 
-calculate_model_comparison_metrics <- function(x) {
-  modelnames <- as.character(x$model)
-  ## Log-likelihood
-  LL = -2 * x$loglik
-  delta_LL = LL - min(LL)
-  loglik_weights = exp(0.5 * -delta_LL) / sum(exp(0.5 * -delta_LL))
-  loglik_rw  <- loglik_weights %*% t(1/loglik_weights)
-  diag(loglik_rw) = 1
-  
-  ## penalty
-  penalty_weights = exp(-x$penalty) / sum(exp(-x$penalty))
-  penalty_rw = penalty_weights %*% t(1/penalty_weights)
-  diag(penalty_rw) = 1
-  
-  ## goric
-  delta_goric = x$goric - min(x$goric)
-  goric_weights = exp(0.5 * -delta_goric) / sum(exp(0.5 * -delta_goric))
-  goric_rw = goric_weights %*% t(1/goric_weights)
-  diag(goric_rw) = 1
-  
-  rownames(goric_rw)   = modelnames
-  rownames(penalty_rw) = modelnames
-  rownames(loglik_rw)  = modelnames
-  colnames(goric_rw)   = paste0("vs. ", modelnames)
-  colnames(penalty_rw) = paste0("vs. ", modelnames)
-  colnames(loglik_rw)  = paste0("vs. ", modelnames)
-  
-  out <- list(loglik_weights  = loglik_weights, 
-              penalty_weights = penalty_weights,
-              goric_weights   = goric_weights,
-              loglik_rw       = loglik_rw,
-              penalty_rw      = penalty_rw,
-              goric_rw        = goric_rw)
-  
-  return(out)
-}
-
-
 # Function to identify list and corresponding messages
 identify_messages <- function(x) {
   messages_info <- list()
@@ -220,31 +173,6 @@ detect_range_restrictions <- function(Amat) {
   return(range_restrictions)
 }
 
-
-
-PT_Amat_meq <- function(Amat, meq) {
-  # check for range-restrictions and treat them as equalities
-  PT_meq  <- meq
-  # check for linear dependence
-  RREF <- GaussianElimination(t(Amat)) # qr(Amat)$rank
-  # remove linear dependent rows
-  PT_Amat <- Amat[RREF$pivot, , drop = FALSE] 
-  
-  if (nrow(Amat) > 1) {
-    # check for range restrictions, e.g., -1 < beta < 1
-    idx_range_restrictions <- detect_range_restrictions(Amat)
-    # range restrictions are treated as equalities for computing PT (goric)
-    n_range_restrictions <- nrow(idx_range_restrictions)
-    PT_meq <- meq + n_range_restrictions
-    # reorder PT_Amat: ceq first, ciq second, needed for QP.solve()
-    meq_order_idx <- RREF$pivot %in% c(idx_range_restrictions)
-    PT_Amat <- rbind(PT_Amat[meq_order_idx, ], PT_Amat[!meq_order_idx, ])
-  }
-  
-  return(list(PT_meq = PT_meq, PT_Amat = PT_Amat, RREF = RREF))
-}
-
-
 # correct misspecified constraints of format e.g., x1 < 1 & x1 < 2.
 # x1 < 2 is removed since it is redundant. It has no impact on the LPs, but
 # since the redundant matrix is not full row-rank the slower boot method is used. 
@@ -257,55 +185,4 @@ remove_redundant_constraints <- function(constraints, rhs) {
   row.names(df_reduced) <- NULL
   colnames(df_reduced) <- NULL
   list(constraints = as.matrix(df_reduced[, -ncol(df_reduced)]), rhs = rhs) 
-}
-
-
-
-calculate_weight_bar <- function(Amat, meq, VCOV, mix_weights, seed, control,
-                                 verbose, ...) {
-  wt.bar <- NA
-  if (nrow(Amat) == meq) {
-    # equality constraints only
-    wt.bar <- rep(0L, ncol(VCOV) + 1)
-    wt.bar.idx <- ncol(VCOV) - qr(Amat)$rank + 1
-    wt.bar[wt.bar.idx] <- 1
-  } else if (all(c(Amat) == 0)) { 
-    # unrestricted case
-    wt.bar <- c(rep(0L, ncol(VCOV)), 1)
-  } else if (mix_weights == "boot") { 
-    # compute chi-square-bar weights based on Monte Carlo simulation
-    wt.bar <- con_weights_boot(VCOV = VCOV,
-                               Amat = Amat, 
-                               meq  = meq, 
-                               R    = ifelse(is.null(control$mix_weights_bootstrap_limit), 
-                                             1e5L, control$mix_weights_bootstrap_limit),
-                               seed = seed,
-                               convergence_crit = ifelse(is.null(control$convergence_crit), 
-                                                         1e-03, control$convergence_crit),
-                               chunk_size = ifelse(is.null(control$chunk_size), 
-                                                   5000L, control$chunk_size),
-                               verbose = verbose, ...)
-    attr(wt.bar, "mix_weights_bootstrap_limit") <- control$mix_weights_bootstrap_limit 
-  } else if (mix_weights == "pmvnorm" && meq < nrow(Amat)) {
-    # compute chi-square-bar weights based on pmvnorm
-    wt.bar <- rev(con_weights(Amat %*% VCOV %*% t(Amat), meq = meq))
-    
-    # Check if wt.bar contains NaN values
-    if (any(is.nan(wt.bar))) {
-      mix_weights <- "boot"
-      wt.bar <- con_weights_boot(VCOV = VCOV,
-                                 Amat = Amat, 
-                                 meq  = meq, 
-                                 R    = ifelse(is.null(control$mix_weights_bootstrap_limit), 
-                                               1e5L, control$mix_weights_bootstrap_limit),
-                                 seed = seed,
-                                 convergence_crit = ifelse(is.null(control$convergence_crit), 
-                                                           1e-03, control$convergence_crit),
-                                 chunk_size = ifelse(is.null(control$chunk_size), 
-                                                     5000L, control$chunk_size),
-                                 verbose = verbose, ...)
-      attr(wt.bar, "mix_weights_bootstrap_limit") <- control$mix_weights_bootstrap_limit   
-    } 
-  }
-  return(wt.bar)
 }

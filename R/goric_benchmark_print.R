d89905aa99f4092a8516611e95990a1202d393a5
LeonardV <lgf.vanbrabant@gmail.com>
2024-10-31 16:31:49 +0100

    add hypothesis rate to print function

--- R/goric_benchmark_print.R
+++ R/goric_benchmark_print.R
@@ -1,5 +1,5 @@
 print.benchmark <- function(x, output_type = c("rgw", "gw", "rlw", "ld", "all"), 
-                            color = TRUE, ...) {
+                            hypo_rate_threshold = 1, color = TRUE, ...) {
 
   # Ensure the object is of class 'benchmark_means'
   if (!inherits(x, "benchmark")) {
@@ -9,6 +9,10 @@ print.benchmark <- function(x, output_type = c("rgw", "gw", "rlw", "ld", "all"),
   output_type <- match.arg(output_type, c("rgw", "gw", "rlw", "ld", "all"))
   
   ldots <- list(...)
+  
+  # compute hypothesis rate
+  x$hypothesis_rate <- lapply(x$combined_values$rgw_combined, 
+                              calculate_hypothesis_rate, q = hypo_rate_threshold)
   
   # number of failed bootstrap runs
   empty_lists_count <- sapply(x$combined_values$gw_combined, function(x) { 
@@ -67,6 +71,7 @@ print.benchmark <- function(x, output_type = c("rgw", "gw", "rlw", "ld", "all"),
       reset <- "}"
     }
   } else {
+    # R Markdown cannot handle color codes (console, html and latex)
     blue <- green <- orange <- reset <- ""
   }
   
@@ -80,7 +85,7 @@ print.benchmark <- function(x, output_type = c("rgw", "gw", "rlw", "ld", "all"),
   cat(paste0(blue, "Benchmark Results", reset), "\n")
   cat(strrep("-", 70), "\n")
   cat(sprintf("Preferred Hypothesis: %s%s%s\n", green, pref_hypo, reset))
-  cat(sprintf("Error probability Preferred Hypothesis vs. complement: %s%s%s\n", green, error_prob_pref_hypo, reset))
+  cat(sprintf("Error probability Preferred Hypothesis vs. Complement: %s%s%s\n", green, error_prob_pref_hypo, reset))
   if (inherits(x, "benchmark_means")) {
     cat(sprintf("Number of Groups: %s%s%s\n", green, ngroups, reset))
   } else {
@@ -113,9 +118,7 @@ print.benchmark <- function(x, output_type = c("rgw", "gw", "rlw", "ld", "all"),
                       "it is advisable to increase the number of bootstrap iterations.")
     message("---\n", text_msg)
   }
-# -------------------------------------------------------------------------
 
-  
   # Normalize output_type to lowercase
   output_type <- tolower(output_type)
   
@@ -125,7 +128,8 @@ print.benchmark <- function(x, output_type = c("rgw", "gw", "rlw", "ld", "all"),
       text_gw,
       function() {
         for (pop_es in names(x$benchmarks_goric_weights)) {
-          print_rounded_es_value(x$benchmarks_goric_weights[[pop_es]], pop_es, model_type, green, reset)
+          print_rounded_es_value(x$benchmarks_goric_weights[[pop_es]], pop_es, 
+                                 model_type, green, reset)
         }
       }, nchar(text_gw), text_color = blue, reset = reset
     )
@@ -133,11 +137,21 @@ print.benchmark <- function(x, output_type = c("rgw", "gw", "rlw", "ld", "all"),
   
   # Print benchmarks for ratio Goric-weights percentiles
   if ("all" %in% output_type || "rgw" %in% output_type) {
+    
+    # Loop over alle sets in both benchmarks_ratio_goric_weights and hypothesis_rate
+    for (pop_es_name in names(x$benchmarks_ratio_goric_weights)) {
+      x$benchmarks_ratio_goric_weights[[pop_es_name]] <- cbind(
+        x$benchmarks_ratio_goric_weights[[pop_es_name]],
+        hypothesis_rate = x$hypothesis_rate[[pop_es_name]] # x$hypothesis_rate
+      )
+    }
+    
     print_section(
       text_rgw,
       function() {
         for (pop_es in names(x$benchmarks_ratio_goric_weights)) {
-          print_rounded_es_value(x$benchmarks_ratio_goric_weights[[pop_es]], pop_es, model_type, green, reset)
+          print_rounded_es_value(x$benchmarks_ratio_goric_weights[[pop_es]], pop_es, 
+                                 model_type, green, reset)
         }
       }, nchar(text_rgw), text_color = blue, reset = reset
     )
@@ -149,7 +163,8 @@ print.benchmark <- function(x, output_type = c("rgw", "gw", "rlw", "ld", "all"),
       text_rlw,
       function() {
         for (pop_es in names(x$benchmarks_ratio_ll_weights)) {
-          print_rounded_es_value(x$benchmarks_ratio_ll_weights[[pop_es]], pop_es, model_type, green, reset)
+          print_rounded_es_value(x$benchmarks_ratio_ll_weights[[pop_es]], pop_es, 
+                                 model_type, green, reset)
         }
       }, nchar(text_rlw), text_color = blue, reset = reset
     )
@@ -161,9 +176,11 @@ print.benchmark <- function(x, output_type = c("rgw", "gw", "rlw", "ld", "all"),
       text_ld,
       function() {
         for (pop_es in names(x$benchmarks_difLL)) {
-          print_rounded_es_value(x$benchmarks_difLL[[pop_es]], pop_es, model_type, green, reset)
+          print_rounded_es_value(x$benchmarks_difLL[[pop_es]], pop_es, model_type, 
+                                 green, reset)
         }
       }, nchar(text_ld), text_color = blue, reset = reset
     )
   }
+  return(invisible(x))
 }

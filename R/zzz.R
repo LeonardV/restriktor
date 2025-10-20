9221c74e4d4ff5bc28ac13700304643e55b85b3c
LeonardV <lgf.vanbrabant@gmail.com>
2025-10-20 16:27:16 +0200

    add global variables

--- R/zzz.R
+++ R/zzz.R
@@ -5,7 +5,8 @@
     packageStartupMessage("Please report any bugs to info@restriktor.org")
 }
 
-utils::globalVariables(c("Group", "Value", "green", "reset", 
+utils::globalVariables(c("green", "reset", "Value", 
                          "Group_hypo_comparison", "Group_pop_values", 
-                         "sample_value", "x", "y", "percentile_value",
-                         "percentile_label"))
+                         "sample_value", "percentile_value",
+                         "percentile_label", "Group"
+                         ))
